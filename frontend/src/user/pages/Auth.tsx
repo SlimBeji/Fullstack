3b5cfd0bb54b41cdf30cf51e870fc07a1e8dce9f
Slim Beji <mslimbeji@gmail.com>
2025-03-24 10:08:26 +0100

    implementing signing up and signing in in frontend

--- frontend/src/user/pages/Auth.tsx
+++ frontend/src/user/pages/Auth.tsx
@@ -4,7 +4,7 @@ import { useState, useContext } from "react";
 
 import { AuthContext } from "../../shared/context";
 import { useForm, emptyStateBuilder } from "../../shared/hooks/form";
-import { Card } from "../../shared/components/ui";
+import { Card, ErrorModal, LoadingSpinner } from "../../shared/components/ui";
 import { Input, Button } from "../../shared/components/form";
 
 import {
@@ -13,6 +13,7 @@ import {
     minValidator,
 } from "../../shared/util/validators";
 import { backendApi } from "../../shared/util/axios";
+import { AxiosError, HttpStatusCode } from "axios";
 
 const AuthForm = {
     username: false,
@@ -27,6 +28,8 @@ const emptyState = emptyStateBuilder<AuthFormTypes>(AuthForm);
 const Auth: React.FC = () => {
     const auth = useContext(AuthContext);
     const [isLoginMode, setIsLoginMode] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const [state, inputHandlers, _, fieldsActivationHandler] =
         useForm<AuthFormTypes>(emptyState);
 
@@ -49,13 +52,12 @@ const Auth: React.FC = () => {
                 name: state.inputs.username.val,
                 password: state.inputs.password.val,
             })
-            .then((res) => {
-                console.log(res);
+            .then(() => {
+                auth.login();
             })
             .catch((err) => {
-                console.log(err);
+                errorHandler(err);
             });
-        auth.login();
     };
 
     const onSignup = (): void => {
@@ -65,22 +67,34 @@ const Auth: React.FC = () => {
                 email: state.inputs.email.val,
                 password: state.inputs.password.val,
             })
-            .then((res) => {
-                console.log(res);
+            .then(() => {
+                auth.login();
             })
             .catch((err) => {
-                console.log(err);
+                errorHandler(err);
             });
-        auth.login();
+    };
+
+    const errorHandler = (err: Error) => {
+        const defaultMessage = "Something went wrong!";
+        if (err instanceof AxiosError) {
+            const message = err.response?.data?.message;
+            setErrorMessage(message || defaultMessage);
+        } else {
+            setErrorMessage(err.message || defaultMessage);
+        }
     };
 
     const onSubmit = (e: React.FormEvent): void => {
         e.preventDefault();
+        setIsLoading(true);
+        setErrorMessage("");
         if (isLoginMode) {
             onSignin();
         } else {
             onSignup();
         }
+        setIsLoading(false);
     };
 
     const onSwitchModeHandler = () => {
@@ -93,48 +107,56 @@ const Auth: React.FC = () => {
     };
 
     return (
-        <Card className="authentication">
-            <h2>{requiredText}</h2>
-            <hr />
-            <form onSubmit={onSubmit}>
-                {!isLoginMode && (
+        <>
+            <ErrorModal
+                header="Credentials not valid!"
+                error={errorMessage}
+                onClear={() => setErrorMessage("")}
+            />
+            <Card className="authentication">
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2>{requiredText}</h2>
+                <hr />
+                <form onSubmit={onSubmit}>
+                    {!isLoginMode && (
+                        <Input
+                            onInput={inputHandlers.username}
+                            element="input"
+                            id="username"
+                            type="text"
+                            label="Username"
+                            validators={[minValidator(8)]}
+                            errorText="Please enter a valid username of at least 8 characters"
+                            value={state.inputs.username.val || ""}
+                        />
+                    )}
                     <Input
-                        onInput={inputHandlers.username}
+                        onInput={inputHandlers.email}
                         element="input"
-                        id="username"
-                        type="text"
-                        label="Username"
-                        validators={[minValidator(8)]}
-                        errorText="Please enter a valid username of at least 8 characters"
-                        value={state.inputs.username.val || ""}
+                        id="email"
+                        type="email"
+                        label="E-Mail"
+                        validators={[emailValidator()]}
+                        errorText="Please enter a valid email"
                     />
-                )}
-                <Input
-                    onInput={inputHandlers.email}
-                    element="input"
-                    id="email"
-                    type="email"
-                    label="E-Mail"
-                    validators={[emailValidator()]}
-                    errorText="Please enter a valid email"
-                />
-                <Input
-                    onInput={inputHandlers.password}
-                    element="input"
-                    id="password"
-                    type="password"
-                    label="Password"
-                    validators={[minLengthValidator(10)]}
-                    errorText="Please enter a password with at least 10 characters"
-                />
-                <Button type="submit" disabled={!state.isValid}>
-                    {verb}
+                    <Input
+                        onInput={inputHandlers.password}
+                        element="input"
+                        id="password"
+                        type="password"
+                        label="Password"
+                        validators={[minLengthValidator(10)]}
+                        errorText="Please enter a password with at least 10 characters"
+                    />
+                    <Button type="submit" disabled={!state.isValid}>
+                        {verb}
+                    </Button>
+                </form>
+                <Button inverse onClick={onSwitchModeHandler}>
+                    {switchText}
                 </Button>
-            </form>
-            <Button inverse onClick={onSwitchModeHandler}>
-                {switchText}
-            </Button>
-        </Card>
+            </Card>
+        </>
     );
 };
 
