89c1cb8bd3fbd8e193dc07415f3b36ae2add26e2
Slim Beji <mslimbeji@gmail.com>
2025-03-24 10:07:52 +0100

    update error modal to customize header text

--- frontend/src/shared/components/ui/ErrorModal.tsx
+++ frontend/src/shared/components/ui/ErrorModal.tsx
@@ -5,14 +5,15 @@ import { Button } from "../form";
 
 interface ErrorModelProps {
     error: string;
+    header?: string;
     onClear: () => void;
 }
 
-const ErrorModal: React.FC<ErrorModelProps> = ({ error, onClear }) => {
+const ErrorModal: React.FC<ErrorModelProps> = ({ error, header, onClear }) => {
     return (
         <Modal
             onCancel={onClear}
-            header="An Error Occurred!"
+            header={header || "An Error Occurred!"}
             show={!!error}
             footer={<Button onClick={onClear}>Okay</Button>}
         >
