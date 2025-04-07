f6c8a9bc4d2c74e6f0263fe6fd7886bb550beab6
Slim Beji <mslimbeji@gmail.com>
2025-04-07 10:17:14 +0100

    define an error handler typein backend

--- backend/src/framework/types/types.ts
+++ backend/src/framework/types/types.ts
@@ -1,4 +1,7 @@
 import { NextFunction, Response, Request } from "express";
+import { HttpStatus } from "./enums";
+
+export type ErrorHandler = (err: Error) => [HttpStatus, string];
 
 export type RecursivePartial<T> = {
     [P in keyof T]?: T[P] extends object ? RecursivePartial<T[P]> : T[P];
