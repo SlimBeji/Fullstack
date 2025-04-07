2b42c17c585ce992388f89c4abd4926a46e9edc1
Slim Beji <mslimbeji@gmail.com>
2025-04-07 09:42:45 +0100

    fecth user places from backend

--- frontend/src/places/pages/UserPlaces.tsx
+++ frontend/src/places/pages/UserPlaces.tsx
@@ -1,16 +1,44 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 import PlaceList from "../components/PlaceList";
 
+import { ErrorModal, LoadingSpinner } from "../../shared/components/ui";
 import { Place } from "../../shared/types";
-import { DUMMY_PLACES } from "./data";
+import { useHttp } from "../../shared/hooks";
 
 const UserPlaces: React.FC = () => {
+    const [data, sendRequest, clearError] = useHttp();
     const { userId } = useParams();
-    const places = DUMMY_PLACES.filter(
-        (p: Place) => String(p.creatorId) === userId
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+            } catch (err) {}
+            await sendRequest(`/users/${userId}/places`, "get");
+        };
+        fetchPlaces();
+    }, [sendRequest, userId]);
+
+    const renderPlaces = (): React.JSX.Element | undefined => {
+        if (!data.loading && data.data?.parsed) {
+            const places = data.data.parsed as Place[];
+            return <PlaceList items={places} />;
+        }
+    };
+
+    return (
+        <>
+            {data.error && (
+                <ErrorModal error={data.error.message} onClear={clearError} />
+            )}
+            {data.loading && (
+                <div className="center">
+                    <LoadingSpinner asOverlay />
+                </div>
+            )}
+            {renderPlaces()}
+        </>
     );
-    return <PlaceList items={places} />;
 };
 
 export default UserPlaces;
