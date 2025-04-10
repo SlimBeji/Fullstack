2d433daf0031332ae8913bbf8cee13cb29eacd7a
Slim Beji <mslimbeji@gmail.com>
2025-04-10 10:19:30 +0100

    handle case when no place found for a user in frontend

--- frontend/src/places/pages/UserPlaces.tsx
+++ frontend/src/places/pages/UserPlaces.tsx
@@ -6,22 +6,26 @@ import PlaceList from "../components/PlaceList";
 import { ErrorModal, LoadingSpinner } from "../../shared/components/ui";
 import { Place } from "../../shared/types";
 import { useHttp } from "../../shared/hooks";
+import { HttpStatusCode } from "axios";
 
 const UserPlaces: React.FC = () => {
-    const [data, sendRequest, clearError] = useHttp();
+    const [data, sendRequest, clearError] = useHttp({ ignoreNotFound: true });
     const { userId } = useParams();
     useEffect(() => {
         const fetchPlaces = async () => {
             try {
+                await sendRequest(`/users/${userId}/places`, "get");
             } catch (err) {}
-            await sendRequest(`/users/${userId}/places`, "get");
         };
         fetchPlaces();
     }, [sendRequest, userId]);
 
     const renderPlaces = (): React.JSX.Element | undefined => {
         if (!data.loading && data.data?.parsed) {
-            const places = data.data.parsed as Place[];
+            let places = data.data.parsed as Place[];
+            if (data.statusCode === HttpStatusCode.NotFound) {
+                places = [];
+            }
             return <PlaceList items={places} />;
         }
     };
