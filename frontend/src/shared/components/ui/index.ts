26ded23577efde73be19c62a8c9f95f4c2b6c132
Slim Beji <mslimbeji@gmail.com>
2025-04-23 09:23:17 +0100

    add missing import in the shared components

--- frontend/src/shared/components/ui/index.ts
+++ frontend/src/shared/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Card } from "./Card";
 export { default as Map } from "./Map";
 export { default as ErrorModal } from "./ErrorModal";
 export { default as LoadingSpinner } from "./LoadingSpinner";
+export { default as Modal } from "./Modal";
