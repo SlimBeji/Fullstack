654e177f42187fbb47e3efc023663109e41d6980
Slim Beji <mslimbeji@gmail.com>
2025-05-14 19:58:35 +0100

    delete deprecated types in framework folder

--- backend/src/framework/types/types.ts
+++ backend/src/framework/types/types.ts
@@ -1,11 +1,4 @@
 import { NextFunction, Response, Request } from "express";
-import { HttpStatus } from "../../enums";
-
-export type ErrorHandler = (err: Error) => [HttpStatus, string];
-
-export type RecursivePartial<T> = {
-    [P in keyof T]?: T[P] extends object ? RecursivePartial<T[P]> : T[P];
-};
 
 export type ParsedRequest<T = any> = Request & {
     parsed: T;
