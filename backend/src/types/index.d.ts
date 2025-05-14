701332728bf4dfa9630110763353ccb1d468ae26
Slim Beji <mslimbeji@gmail.com>
2025-05-14 11:02:30 +0100

    extend express request object to store the currentUser in authentication

--- backend/src/types/index.d.ts
+++ backend/src/types/index.d.ts
@@ -1,6 +1,11 @@
 import "express";
+import { User } from "../schemas";
 
 declare module "express-serve-static-core" {
+    interface Request {
+        currentUser?: User;
+    }
+
     interface Application {
         env?: string;
     }
