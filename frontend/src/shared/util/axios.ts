ff7e33485d3ba8375b239c40ff7e32776eeaa054
Slim Beji <mslimbeji@gmail.com>
2025-05-14 18:44:25 +0100

    update axios client to use the Bearer Token if found

--- frontend/src/shared/util/axios.ts
+++ frontend/src/shared/util/axios.ts
@@ -1,12 +1,19 @@
 import axios, { AxiosInstance } from "axios";
 import { HeaderContent } from "../types";
+import { getToken } from "./storage";
 
 export const getClient = (
     contentType: HeaderContent = "application/json"
 ): AxiosInstance => {
+    const token = getToken();
+    const headers = {
+        "Content-Type": contentType,
+        ...(token && { Authorization: token }),
+    };
+
     return axios.create({
         baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
-        headers: { "Content-Type": contentType },
+        headers,
     });
 };
 
