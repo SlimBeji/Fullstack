aea4bd2495e5867b63c0bde0ebdcf9328f2f786b
Slim Beji <mslimbeji@gmail.com>
2025-05-27 10:36:37 +0100

    refactoring index.ts: putting app in app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import "express-async-errors";
+import express from "express";
+import cookieSession from "cookie-session";
+
+import config from "./config";
+import { errorHandler, cors, noRouteMatchHandler } from "./middlewares";
+import { registerRoutes } from "./routes";
+
+const app = express();
+app.env = config.ENV;
+
+app.use(cors);
+app.use(express.json({ limit: config.JSON_MAX_SIZE }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: [config.SECRET_KEY] }));
+registerRoutes(app);
+app.all("*", noRouteMatchHandler);
+app.use(errorHandler);
+
+export default app;
