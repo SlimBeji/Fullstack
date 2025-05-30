246302764020028777017f2bd71d1323799d3347
Slim Beji <mslimbeji@gmail.com>
2025-05-30 16:17:28 +0100

    remove ole script

--- express/src/scripts/dbIndex.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import config from "../config";
-
-mongoose
-    .connect(config.MONGO_URL)
-    .then(() => mongoose.connection.db!.collection("places").indexes())
-    .then((indexes: any) => console.log(indexes))
-    .finally(() => mongoose.disconnect());
