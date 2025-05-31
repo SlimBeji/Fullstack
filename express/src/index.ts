99ea59269592574a1718545c52697ea0403036bb
Slim Beji <mslimbeji@gmail.com>
2025-05-31 13:09:01 +0100

    fix test failure

--- express/src/index.ts
+++ express/src/index.ts
@@ -3,13 +3,15 @@ import config from "./config";
 import app from "./api";
 
 // Connect Mongoose than run the app
-mongoose
-    .connect(config.MONGO_URL!, { dbName: config.MONGO_DBNAME })
-    .then(() => {
-        app.listen(config.PORT, () => {
-            console.log(`Listening on port ${config.PORT}`);
+if (require.main === module) {
+    mongoose
+        .connect(config.MONGO_URL!, { dbName: config.MONGO_DBNAME })
+        .then(() => {
+            app.listen(config.PORT, () => {
+                console.log(`Listening on port ${config.PORT}`);
+            });
+        })
+        .catch(() => {
+            console.log("Could not stablish connection to database");
         });
-    })
-    .catch(() => {
-        console.log("Could not stablish connection to database");
-    });
+}
