61cd51e6a06981766d029ce86a0abf30d83e9730
Slim Beji <mslimbeji@gmail.com>
2025-06-03 19:21:44 +0100

    test missing endpoints from the auth endpoints

--- express/src/tests/api/hello.test.ts
+++ express/src/tests/api/hello.test.ts
@@ -1,6 +1,8 @@
 import supertest from "supertest";
 import { memoryDb } from "../memoryDb";
 import app from "../../api";
+import { crudUser } from "../../models/crud";
+
 const request = supertest(app);
 
 beforeAll(async () => {
@@ -19,5 +21,31 @@ describe("GET /api/hello-world/", () => {
             .expect(200);
 
         expect(response.body).toEqual({ message: "Hello World!" });
+    });
+});
+
+describe("GET /api/hello-world/user", () => {
+    it("responds with json", async () => {
+        const token = await crudUser.getBearer("mslimbeji@gmail.com");
+        const response = await request
+            .get("/api/hello-world/user")
+            .set("Authorization", token)
+            .expect("Content-Type", /json/)
+            .expect(200);
+
+        expect(response.body).toEqual({ message: "Hello Slim Beji!" });
+    });
+});
+
+describe("GET /api/hello-world/admin", () => {
+    it("responds with json", async () => {
+        const token = await crudUser.getBearer("mslimbeji@gmail.com");
+        const response = await request
+            .get("/api/hello-world/admin")
+            .set("Authorization", token)
+            .expect("Content-Type", /json/)
+            .expect(200);
+
+        expect(response.body).toEqual({ message: "Hello Admin Slim Beji!" });
     });
 });
