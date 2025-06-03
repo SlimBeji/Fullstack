da8aec9e14dac12b5b147390f151abff6b64acec
Slim Beji <mslimbeji@gmail.com>
2025-06-03 12:05:41 +0100

    document the signin endpoint

--- express/src/api/routes/auth.ts
+++ express/src/api/routes/auth.ts
@@ -5,21 +5,53 @@ import {
     SigninForm,
     SignupBodyForm,
     SigninSchema,
-    SignupForm,
     SignupSchema,
+    EncodedTokenSchema,
 } from "../../models/schemas";
 import { storage } from "../../lib/utils";
 import { ApiError, HttpStatus } from "../../types";
+import { swaggerRegistery } from "../openapi";
 
 export const authRouter = Router();
 
+// Signin in route
 async function signin(req: Request, res: Response, next: NextFunction) {
     const parsed = req.parsed as SigninForm;
     const tokenData = await crudUser.signin(parsed);
     res.status(200).json(tokenData);
 }
+
 authRouter.post("/signin", validateBody(SigninSchema), signin);
 
+swaggerRegistery.registerPath({
+    method: "post",
+    path: "/auth/signin",
+    request: {
+        body: {
+            content: {
+                "application/json": {
+                    schema: SigninSchema,
+                },
+            },
+            description: "user credentials",
+            required: true,
+        },
+    },
+    responses: {
+        200: {
+            description: "User logged in and received his access token",
+            content: {
+                "application/json": {
+                    schema: EncodedTokenSchema,
+                },
+            },
+        },
+    },
+    tags: ["Auth"],
+    summary: "User authentication",
+});
+
+// Signup route
 async function signup(req: Request, res: Response, next: NextFunction) {
     const parsed = req.parsed as SignupBodyForm;
     const duplicateMsg = await crudUser.checkDuplicate(
