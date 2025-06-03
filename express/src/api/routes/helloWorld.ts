06929714908721331ccad2d778b01f9b7da4c283
Slim Beji <mslimbeji@gmail.com>
2025-06-03 15:40:30 +0100

    add hello-world to swagger

--- express/src/api/routes/helloWorld.ts
+++ express/src/api/routes/helloWorld.ts
@@ -1,13 +1,37 @@
 import { Router, Request, Response, NextFunction } from "express";
-
+import { z } from "../../zod";
 import { Authenticated, Admin } from "../middlewares";
+import { swaggerRegistery } from "../openapi";
 
 export const helloWorldRouter = Router();
 
+// Hello World Endpoint
 async function hello(req: Request, res: Response, next: NextFunction) {
     res.status(200).json({ message: "Hello World!" });
 }
+
 helloWorldRouter.get("/", hello);
+
+swaggerRegistery.registerPath({
+    method: "get",
+    path: "/hello-world",
+    responses: {
+        200: {
+            description: "A simple response with message field",
+            content: {
+                "application/json": {
+                    schema: z.object({
+                        message: z.string().openapi({
+                            example: "Hello World",
+                        }),
+                    }),
+                },
+            },
+        },
+    },
+    tags: ["Hello World!"],
+    summary: "Hello World Endpoint",
+});
 
 async function helloUser(req: Request, res: Response, next: NextFunction) {
     res.status(200).json({ message: `Hello ${req.currentUser?.name}!` });
