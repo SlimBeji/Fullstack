1e415661f74db91c5d482d930403b63a98fdb47c
Slim Beji <mslimbeji@gmail.com>
2025-06-05 15:56:54 +0100

    create a helper to parse object with keys using dot notations into a nested format

--- express/src/lib/utils/helpers.ts
+++ express/src/lib/utils/helpers.ts
@@ -1,3 +1,29 @@
+export const parseDotNotation = (data: any): any => {
+    const result: any = {};
+
+    for (const dotKey in data) {
+        if (!Object.prototype.hasOwnProperty.call(data, dotKey)) {
+            continue; // Ensure it's an own property from the input data
+        }
+
+        const parts = dotKey.split(".");
+        let current: { [key: string]: any } = result;
+        for (let i = 0; i < parts.length - 1; i++) {
+            const part = parts[i];
+            if (!(part in current)) {
+                current[part] = {};
+            }
+            current = current[part];
+        }
+
+        // Assign the original value and operation object to the deepest (last) part
+        const lastPart = parts[parts.length - 1];
+        current[lastPart] = data[dotKey];
+    }
+
+    return result;
+};
+
 export const getImagePath = (p: string) => {
     return `src/static/images/${p}`;
 };
---
+++ express/src/tests/utils/helpers.test.ts
@@ -0,0 +1,16 @@
+import { parseDotNotation } from "../../lib/utils";
+
+describe("parseDotNotation helper method", () => {
+    it("converts flat json with keys using dot notation to a nested object", () => {
+        const example = {
+            address: "Fulham Road, London",
+            "location.lat": 51.48180425016331,
+            "location.lng": -0.19090418688755467,
+        };
+        const expected = {
+            address: "Fulham Road, London",
+            location: { lat: 51.48180425016331, lng: -0.19090418688755467 },
+        };
+        expect(parseDotNotation(example)).toEqual(expected);
+    });
+});
