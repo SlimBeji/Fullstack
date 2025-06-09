316858fde4929012ac610f1b47a32025cb5f9816
Slim Beji <mslimbeji@gmail.com>
2025-06-09 08:58:55 +0100

    updating the places endpoints

--- express/src/api/routes/places.ts
+++ express/src/api/routes/places.ts
@@ -2,35 +2,33 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import { crudPlace } from "../../models/crud";
 import {
-    PlacePostBody,
+    PlaceSearchSchema,
+    PlaceSortableFields,
+    PlacePost,
     PlacePostSchema,
     PlacePut,
     PlacePutSchema,
-    PlaceSearchSchema,
-    PlaceSortableFields,
 } from "../../models/schemas";
 import { extractFile, fileUpload, validateBody, filter } from "../middlewares";
-import { storage } from "../../lib/utils";
-import { ApiError, HttpStatus } from "../../types";
 
 export const placeRouter = Router();
 
+// Get Places Endpoint
 async function getPlaces(req: Request, resp: Response, next: NextFunction) {
     const query = req.filterQuery!;
-    resp.status(200).json(await crudPlace.search(query));
+    resp.status(200).json(await crudPlace.fetch(query));
 }
+
 placeRouter.get("/", filter(PlaceSearchSchema, PlaceSortableFields), getPlaces);
 
+// Post New Places
 async function createPlace(req: Request, resp: Response, next: NextFunction) {
-    const parsed = req.parsed as PlacePostBody;
-    const imageFile = extractFile(req, "image");
-    if (!imageFile) {
-        throw new ApiError(HttpStatus.BAD_REQUEST, "No Image was provided");
-    }
-    const imageUrl = await storage.uploadFile(imageFile);
-    const newPlace = await crudPlace.create({ ...parsed, imageUrl });
+    const parsed = req.parsed as PlacePost;
+    const image = extractFile(req, "image") || undefined;
+    const newPlace = await crudPlace.create({ ...parsed, image });
     resp.status(200).json(newPlace);
 }
+
 placeRouter.post(
     "/",
     fileUpload([{ name: "image" }]),
@@ -38,24 +36,30 @@ placeRouter.post(
     createPlace
 );
 
+// Get a place by ID
 async function getPlace(req: Request, res: Response, next: NextFunction) {
-    const place = await crudPlace.get(req.params.placeId);
+    const place = await crudPlace.getById(req.params.placeId);
     res.status(200).json(place);
 }
+
 placeRouter.get("/:placeId", getPlace);
 
+// Edit Places
 async function editPlace(req: Request, res: Response, next: NextFunction) {
     const parsed = req.parsed as PlacePut;
-    const place = await crudPlace.get(req.params.placeId);
-    const updatedPlace = await crudPlace.update(place.id, parsed);
+    const place = await crudPlace.getDocument(req.params.placeId);
+    const updatedPlace = await crudPlace.updateDocument(place!, parsed);
     res.status(200).json(updatedPlace);
 }
+
 placeRouter.put("/:placeId", validateBody(PlacePutSchema), editPlace);
 
+// Delete Places
 async function deletePlace(req: Request, res: Response, next: NextFunction) {
-    await crudPlace.delete(req.params.placeId);
+    await crudPlace.deleteById(req.params.placeId);
     res.status(200).json({
         message: `Deleted place ${req.params.placeId}`,
     });
 }
+
 placeRouter.delete("/:placeId", deletePlace);
