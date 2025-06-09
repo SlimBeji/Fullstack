9cedcc37ec3ab17bba47300b4c21e411345b3be7
Slim Beji <mslimbeji@gmail.com>
2025-06-09 08:33:42 +0100

    update the auth routs

--- express/src/api/routes/auth.ts
+++ express/src/api/routes/auth.ts
@@ -2,12 +2,12 @@ import { Router, Response, Request, NextFunction } from "express";
 import { fileUpload, extractFile, validateBody } from "../middlewares";
 import { crudUser } from "../../models/crud";
 import {
-    SigninForm,
-    SignupBodyForm,
+    Signin,
     SigninSchema,
-    SignupSchema,
-    EncodedTokenSchema,
+    SignupBody,
+    SignupBodySchema,
     SignupMultipartSchema,
+    EncodedTokenSchema,
 } from "../../models/schemas";
 import { storage } from "../../lib/utils";
 import { ApiError, HttpStatus } from "../../types";
@@ -17,28 +17,16 @@ export const authRouter = Router();
 
 // Signup route
 async function signup(req: Request, res: Response, next: NextFunction) {
-    const parsed = req.parsed as SignupBodyForm;
-    const duplicateMsg = await crudUser.checkDuplicate(
-        parsed.email,
-        parsed.name
-    );
-    if (duplicateMsg) {
-        throw new ApiError(HttpStatus.BAD_REQUEST, duplicateMsg);
-    }
-
-    let imageUrl = "";
-    const imageFile = extractFile(req, "image");
-    if (imageFile) {
-        imageUrl = await storage.uploadFile(imageFile);
-    }
-    const tokenData = await crudUser.signup({ ...parsed, imageUrl });
+    const parsed = req.parsed as SignupBody;
+    const image = extractFile(req, "image") || undefined;
+    const tokenData = await crudUser.signup({ ...parsed, image });
     res.status(200).json(tokenData);
 }
 
 authRouter.post(
     "/signup",
     fileUpload([{ name: "image" }]),
-    validateBody(SignupSchema),
+    validateBody(SignupBodySchema),
     signup
 );
 
@@ -72,7 +60,7 @@ swaggerRegistery.registerPath({
 
 // Signin in route
 async function signin(req: Request, res: Response, next: NextFunction) {
-    const parsed = req.parsed as SigninForm;
+    const parsed = req.parsed as Signin;
     const tokenData = await crudUser.signin(parsed);
     res.status(200).json(tokenData);
 }
