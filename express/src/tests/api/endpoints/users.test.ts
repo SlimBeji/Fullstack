01bd2eee0e5994a14ac9c379d3084970fe7337d9
Slim Beji <mslimbeji@gmail.com>
2025-06-11 10:02:15 +0100

    update users test to test unauthorized situations

--- express/src/tests/api/endpoints/users.test.ts
+++ express/src/tests/api/endpoints/users.test.ts
@@ -2,19 +2,23 @@ import supertest from "supertest";
 import { memoryDb } from "../../memoryDb";
 import app from "../../../api";
 import { crudUser } from "../../../models/crud";
-import { getImagePath } from "../../../lib/utils";
+import { readImage } from "../../../lib/utils";
 import { UserRead } from "../../../models/schemas";
+import { HttpStatus } from "../../../types";
+import { createToken } from "../../../api/auth";
 
+let adminExample: UserRead;
 let adminToken: string = "";
-let token: string = "";
 let example: UserRead;
+let token: string = "";
 const request = supertest(app);
 
 beforeAll(async () => {
     await memoryDb.session();
-    adminToken = await crudUser.getBearer("mslimbeji@gmail.com");
-    token = await crudUser.getBearer("beji.slim@yahoo.fr");
+    adminExample = (await crudUser.getByEmail("mslimbeji@gmail.com"))!;
+    adminToken = `Bearer ${createToken(adminExample).token}`;
     example = (await crudUser.getByEmail("beji.slim@yahoo.fr"))!;
+    token = `Bearer ${createToken(example).token}`;
 });
 
 afterAll(async () => {
@@ -45,13 +49,26 @@ describe("POST /api/users", () => {
             .field("email", "test@test.com")
             .field("password", "very_secret")
             .field("isAdmin", true)
-            .attach("image", getImagePath("avatar1.jpg"))
+            .attach("image", readImage("avatar1.jpg"))
             .set("Authorization", adminToken)
             .expect("Content-Type", /json/)
             .expect(200);
         expect(response.body).toHaveProperty("email", "test@test.com");
         expect(response.body).toHaveProperty("name", "Test Van Test");
         expect(response.body).toHaveProperty("isAdmin", true);
+    });
+
+    it("Non Admin users are denied", async () => {
+        await request
+            .post("/api/users")
+            .field("name", "Test Van Test II")
+            .field("email", "test_2@test.com")
+            .field("password", "very_secret")
+            .field("isAdmin", true)
+            .attach("image", readImage("avatar1.jpg"))
+            .set("Authorization", token)
+            .expect("Content-Type", /json/)
+            .expect(HttpStatus.UNAUTHORIZED);
     });
 });
 
@@ -79,6 +96,16 @@ describe("PUT /api/users/id", () => {
         expect(response.body).toHaveProperty("email", "beji.slim@yahoo.fr");
         expect(response.body).toHaveProperty("name", "Slim El Beji");
     });
+
+    it("Users cannot update others profiles", async () => {
+        const data = { name: "Slim El Beji" };
+        const response = await request
+            .put(`/api/users/${adminExample.id}`)
+            .send(data)
+            .set("Authorization", token)
+            .expect("Content-Type", /json/)
+            .expect(HttpStatus.UNAUTHORIZED);
+    });
 });
 
 describe("DELETE /api/users/id", () => {
@@ -92,5 +119,13 @@ describe("DELETE /api/users/id", () => {
             "message",
             `Deleted user ${example.id}`
         );
+    });
+
+    it("Only admins can delete Users", async () => {
+        const response = await request
+            .delete(`/api/users/${example.id}`)
+            .set("Authorization", token)
+            .expect("Content-Type", /json/)
+            .expect(HttpStatus.UNAUTHORIZED);
     });
 });
