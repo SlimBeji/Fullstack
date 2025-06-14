c9c5766702c8a494134f607dea5f858d4382c4a5
Slim Beji <mslimbeji@gmail.com>
2025-06-14 12:17:42 +0100

    add flatteJson helper

--- express/src/lib/utils/helpers.ts
+++ express/src/lib/utils/helpers.ts
@@ -26,6 +26,40 @@ export const parseDotNotation = (data: any): any => {
     return result;
 };
 
+export const flattenJson = (
+    obj: Record<string, any>,
+    acceptArrays: boolean = false,
+    prefix = ""
+): Record<string, any> => {
+    const result: Record<string, any> = {};
+    for (const [key, value] of Object.entries(obj)) {
+        const newKey = prefix ? `${prefix}.${key}` : key;
+        if (Array.isArray(value)) {
+            if (!acceptArrays) {
+                throw new Error(
+                    `Array encountered at path '${newKey}' but arrays are not allowed`
+                );
+            }
+            value.forEach((item, index) => {
+                if (typeof item === "object" && item !== null) {
+                    Object.assign(
+                        result,
+                        flattenJson(item, acceptArrays, `${newKey}.${index}`)
+                    );
+                } else {
+                    result[`${newKey}.${index}`] = item;
+                }
+            });
+        } else if (typeof value === "object" && value !== null) {
+            Object.assign(result, flattenJson(value, acceptArrays, newKey));
+        } else {
+            result[newKey] = value;
+        }
+    }
+
+    return result;
+};
+
 export const getImagePath = (p: string) => {
     return `/app/src/static/images/${p}`;
 };
