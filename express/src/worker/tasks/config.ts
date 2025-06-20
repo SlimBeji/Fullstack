f782d57ffa07d87ab0ffe1272460ee1e699f9365
Slim Beji <mslimbeji@gmail.com>
2025-06-20 15:43:07 +0100

    add expiration to bullmq keys

--- express/src/worker/tasks/config.ts
+++ express/src/worker/tasks/config.ts
@@ -2,4 +2,14 @@ import { env } from "../../config";
 
 export const config = {
     connection: { url: env.REDIS_URL },
+    defaultJobOptions: {
+        removeOnComplete: {
+            age: 7 * 24 * 60 * 60 * 1000,
+            count: 10000,
+        },
+        removeOnFail: {
+            age: 30 * 24 * 60 * 60 * 1000,
+            count: 5000,
+        },
+    },
 };
