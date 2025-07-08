79cb35c27568f60fccf87b4a6dccc9d324f74156
Slim Beji <mslimbeji@gmail.com>
2025-07-08 11:30:39 +0100

    create a custom HttpError component

---
+++ react/src/components/ui/HttpError.tsx
@@ -0,0 +1,51 @@
+import { AxiosResponse } from "axios";
+import ErrorModal from "./ErrorModal";
+import Modal from "./Modal";
+import { Button } from "../form";
+import { useNavigate } from "react-router-dom";
+import { authSlice, useAppDispatch } from "../../states";
+
+interface HttpErrorProps {
+    error: {
+        tokenExpired?: boolean;
+        message: string;
+        response?: AxiosResponse;
+    };
+    onClear: () => void;
+    header?: string;
+}
+
+const HttpError: React.FC<HttpErrorProps> = ({ error, onClear, header }) => {
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const tokenExpiredCleaner = () => {
+        onClear();
+        dispatch(authSlice.actions.logout());
+        navigate("/auth");
+        console.log("I was executed");
+    };
+
+    if (!error.tokenExpired) {
+        return (
+            <ErrorModal
+                error={error.message}
+                header={header}
+                onClear={onClear}
+            />
+        );
+    }
+
+    return (
+        <Modal
+            onCancel={tokenExpiredCleaner}
+            header="Session Expired"
+            show={!!error}
+            footer={<Button onClick={tokenExpiredCleaner}>Authenticate</Button>}
+        >
+            <p>Token expired! Please login again</p>
+        </Modal>
+    );
+};
+
+export default HttpError;
--- react/src/components/ui/index.ts
+++ react/src/components/ui/index.ts
@@ -2,5 +2,6 @@ export { default as Avatar } from "./Avatar";
 export { default as Card } from "./Card";
 export { default as Map } from "./Map";
 export { default as ErrorModal } from "./ErrorModal";
+export { default as HttpError } from "./HttpError";
 export { default as LoadingSpinner } from "./LoadingSpinner";
 export { default as Modal } from "./Modal";
