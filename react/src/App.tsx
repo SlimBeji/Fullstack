e29c86676c5a848d29ec7fcfdac818061bfb5d33
Slim Beji <mslimbeji@gmail.com>
2025-07-10 18:15:05 +0100

    app routing

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -4,6 +4,7 @@ import {
     Routes,
     BrowserRouter as Router,
     Navigate,
+    useNavigate,
 } from "react-router-dom";
 
 import { NewPlace, UserPlaces, UpdatePlace, Users, Auth } from "./pages";
@@ -11,7 +12,22 @@ import { MainNavigation } from "./components/navigation";
 import { getAuthData } from "./util";
 import { authSlice, useAppDispatch, useAppSelector } from "./states";
 
-function App() {
+const route = (
+    path: string,
+    component: React.ReactNode,
+    active: boolean = true,
+    alt: string = "/"
+) => {
+    return (
+        <Route
+            path={path}
+            element={active ? component : <Navigate to={alt} replace />}
+        />
+    );
+};
+
+const AppRoutes: React.FC = () => {
+    const navigate = useNavigate();
     const dispatch = useAppDispatch();
     const authData = useAppSelector((state) => state.auth.data);
 
@@ -20,34 +36,43 @@ function App() {
         if (data) dispatch(authSlice.actions.login(data));
     }, [dispatch, authSlice.actions.login]);
 
-    let routes: React.JSX.Element;
-    if (authData) {
-        routes = (
-            <>
-                <Route path="/" element={<Users />} />
-                <Route path="/:userId/places" element={<UserPlaces />} />
-                <Route path="/places/new" element={<NewPlace />} />
-                <Route path="/places/:placeId" element={<UpdatePlace />} />
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </>
-        );
-    } else {
-        routes = (
-            <>
-                <Route path="/" element={<Users />} />
-                <Route path="/:userId/places" element={<UserPlaces />} />
-                <Route path="/auth" element={<Auth />} />
-                <Route path="/*" element={<Navigate to="/" replace />} />
-            </>
-        );
-    }
+    useEffect(() => {
+        if (!authData) {
+            navigate("/auth");
+        }
+    }, [authData]);
 
+    return (
+        <main>
+            <Routes>
+                {/* Unauthenticated Routes */}
+                {route("/auth", <Auth />, !authData, "/")}
+
+                {/* Auth required Routes */}
+                {route("/places/new", <NewPlace />, !!authData, "/auth")}
+                {route(
+                    "/places/:placeId",
+                    <UpdatePlace />,
+                    !!authData,
+                    "/auth"
+                )}
+
+                {/* Public Routes */}
+                {route("/:userId/places", <UserPlaces />)}
+                {route("/", <Users />)}
+
+                {/* Default Route */}
+                {route("*", <Navigate to="/" replace />)}
+            </Routes>
+        </main>
+    );
+};
+
+function App() {
     return (
         <Router>
             <MainNavigation />
-            <main>
-                <Routes>{routes}</Routes>
-            </main>
+            <AppRoutes />
         </Router>
     );
 }
