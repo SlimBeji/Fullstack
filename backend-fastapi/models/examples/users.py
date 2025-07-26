92e8e8a8c718590e8042dd24f9c2585c03fcdf73
Slim Beji <mslimbeji@gmail.com>
2025-07-26 17:20:12 +0100

    create examples for Users and Places

---
+++ backend-fastapi/models/examples/users.py
@@ -0,0 +1,21 @@
+from lib.utils import get_image_path
+from models.schemas import UserSeedSchema
+
+USERS: list[UserSeedSchema] = [
+    UserSeedSchema(
+        _ref=1,
+        name="Slim Beji",
+        email="mslimbeji@gmail.com",
+        password="very_secret",
+        imageUrl=get_image_path("avatar1.jpg"),
+        isAdmin=True,
+    ),
+    UserSeedSchema(
+        _ref=2,
+        name="Mohamed Slim Beji",
+        email="beji.slim@yahoo.fr",
+        password="very_secret",
+        imageUrl=get_image_path("avatar2.jpg"),
+        isAdmin=False,
+    ),
+]
