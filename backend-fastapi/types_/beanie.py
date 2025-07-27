c34fe3ca87658dce2733efcf5d5dbeee70fc53bf
Slim Beji <mslimbeji@gmail.com>
2025-07-27 16:27:42 +0100

    update the types for Beanie/Mongo

--- backend-fastapi/types_/beanie.py
+++ backend-fastapi/types_/beanie.py
@@ -1,5 +1,39 @@
+from typing import Any, Literal
+
 from beanie import Insert, Replace, Update
+from pydantic import BaseModel
+
+from types_.http import PaginationData
 
 SaveEvent = [Insert, Replace, Update]
 
 ChangeEvent = [Replace, Update]
+
+MongoOp = Literal[
+    "$eq", "$ne", "$gt", "$gte", "$lt", "$lte", "$in", "$nin", "$regex", "$text"
+]
+
+FilterOp = Literal["eq", "ne", "gt", "gte", "lt", "lte", "in", "nin", "regex", "text"]
+
+
+ProjectionIncl = dict[str, Literal[0] | "ProjectionIncl"]
+
+ProjectionExcl = dict[str, Literal[0] | "ProjectionExcl"]
+
+SortData = dict[str, Literal[-1, 1]]
+
+MongoFilter = dict[MongoOp, Any]
+
+FilterData = dict[str, MongoFilter]
+
+
+class MongoBaseFilter(BaseModel):
+    op: FilterOp
+    val: list[str]
+
+
+class FilterQuery(BaseModel):
+    pagination: PaginationData | None
+    sort: SortData | None
+    filters: FilterData | None
+    projection: ProjectionIncl | None
