0c9ad37d42d4a0114c92c14f36d190efd8350abb
Slim Beji <mslimbeji@gmail.com>
2025-07-28 17:20:46 +0100

    rebuild models for circular imports

--- backend-fastapi/models/collections/__init__.py
+++ backend-fastapi/models/collections/__init__.py
@@ -2,4 +2,6 @@ from models.collections.base import *
 from models.collections.place import *
 from models.collections.user import *
 
+User.model_rebuild()
+Place.model_rebuild()
 document_models = [User, Place]
