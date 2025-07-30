c8f65d4c1a7d13ab74b7b5ce18301bd8849f1a05
Slim Beji <mslimbeji@gmail.com>
2025-07-30 09:54:30 +0100

    create a helper class for uploading models

--- backend-fastapi/types_/http.py
+++ backend-fastapi/types_/http.py
@@ -1,3 +1,5 @@
+import mimetypes
+import os
 from typing import Generic, TypeVar
 
 from pydantic import BaseModel
@@ -16,3 +18,18 @@ class PaginatedData(BaseModel, Generic[ReadSchema]):
     totalPages: int
     totalCount: int
     data: list[ReadSchema]
+
+
+class FileToUpload:
+    def __init__(self, filename: str, mimetype: str, buffer: bytes) -> None:
+        self.name: str = filename
+        self.mimetype = mimetype
+        self.buffer: bytes = buffer
+
+    @classmethod
+    def from_path(cls, path: str) -> "FileToUpload":
+        filename = os.path.basename(path)
+        mimetype = mimetypes.guess_type(path)[0] or "application/octet-stream"
+        with open(path, "rb") as f:
+            buffer = f.read()
+        return cls(filename, mimetype, buffer)
