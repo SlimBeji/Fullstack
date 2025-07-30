377a3ab840b09ddbe401a37c03524267c9db8860
Slim Beji <mslimbeji@gmail.com>
2025-07-30 19:42:37 +0100

    add values helper to StrEnum and FloatEnum

--- backend-fastapi/types_/enums.py
+++ backend-fastapi/types_/enums.py
@@ -5,10 +5,18 @@ class StrEnum(str, Enum):
     def __str__(self):
         return self.value
 
+    @classmethod
+    def values(cls) -> list[str]:
+        return [e.value for e in cls]
+
 
 class FloatEnum(float, Enum):
     def __str__(self):
         return str(self.value)
+
+    @classmethod
+    def values(cls) -> list[float]:
+        return [e.value for e in cls]
 
 
 class ContentType(StrEnum):
