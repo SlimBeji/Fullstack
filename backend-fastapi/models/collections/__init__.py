faeccc7eca163ab6595cf38663a58577d3d0908e
Slim Beji <mslimbeji@gmail.com>
2025-07-30 19:41:19 +0100

    add missing type annotations

--- backend-fastapi/models/collections/__init__.py
+++ backend-fastapi/models/collections/__init__.py
@@ -1,7 +1,11 @@
+from typing import Type
+
+from beanie import Document
+
 from models.collections.base import *
 from models.collections.place import *
 from models.collections.user import *
 
 User.model_rebuild()
 Place.model_rebuild()
-document_models = [User, Place]
+document_models: list[Type[Document]] = [User, Place]
