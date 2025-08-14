0e92c95cf83f7e13e3c94936d3bbe390cbb8af65
Slim Beji <mslimbeji@gmail.com>
2025-08-14 20:30:09 +0100

    update return type of create token

--- backend-fastapi/api/auth/jwt.py
+++ backend-fastapi/api/auth/jwt.py
@@ -1,5 +1,12 @@
-from models.schemas import UserReadSchema
+import time
+
+from models.schemas import EncodedTokenSchema, UserReadSchema
 
 
-def create_token(user: UserReadSchema) -> str:
-    return f"__TOKEN_FOR_USER_{user.email}__"
+def create_token(user: UserReadSchema) -> EncodedTokenSchema:
+    return EncodedTokenSchema(
+        userId=user.id,
+        email=user.email,
+        token=f"__TOKEN_FOR_USER_{user.email}__",
+        expiresAt=int(time.time()),
+    )
