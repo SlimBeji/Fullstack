8fa8a594e37c13d06edc972711d795d312876527
Slim Beji <mslimbeji@gmail.com>
2025-08-16 20:19:05 +0100

    add signup form

--- backend-fastapi/api/routes/auth.py
+++ backend-fastapi/api/routes/auth.py
@@ -1,9 +1,16 @@
 from fastapi import APIRouter, Depends
 
 from models.crud import crud_user
-from models.schemas import EncodedTokenSchema, SigninForm
+from models.schemas import EncodedTokenSchema, SigninForm, SignupForm
 
 auth_router = APIRouter(prefix="/api/auth", tags=["Auth"])
+
+
+@auth_router.post(
+    "/signup", summary="User registration", response_model=EncodedTokenSchema
+)
+async def signup(form: SignupForm = Depends()):
+    return await crud_user.signup(form)
 
 
 @auth_router.post(
