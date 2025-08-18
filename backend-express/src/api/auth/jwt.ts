ebb3f1b52d91adf9288888b7a06f31b3b4138400
Slim Beji <mslimbeji@gmail.com>
2025-08-18 17:20:27 +0100

    recator jwt express module

--- backend-express/src/api/auth/jwt.ts
+++ backend-express/src/api/auth/jwt.ts
@@ -1,56 +1,10 @@
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { Types } from "mongoose";
-
-import { env } from "../../config";
-import { redisClient } from "../../lib/clients";
+import { decodeToken } from "../../lib/encryption";
 import { crudUser } from "../../models/crud";
-import { EncodedToken, UserRead } from "../../models/schemas";
+import { UserRead } from "../../models/schemas";
 import { ApiError, HttpStatus } from "../../types";
 
-export interface UserTokenInput {
-    userId: Types.ObjectId;
-    email: string;
-}
-
-export interface DecodedUserToken extends UserTokenInput, JwtPayload {}
-
-const _createToken = (user: UserRead): EncodedToken => {
-    const payload: UserTokenInput = {
-        userId: user.id,
-        email: user.email,
-    };
-    const token = jwt.sign(payload, env.SECRET_KEY, {
-        expiresIn: env.JWT_EXPIRATION,
-    });
-    const decoded = jwt.decode(token) as JwtPayload;
-    return {
-        token,
-        email: user.email,
-        userId: user.id,
-        expiresAt: Number(decoded.exp),
-    };
-};
-
-const createTokenKeygen = (user: UserRead): string => {
-    return `create_token_${user.email}`;
-};
-
-export const createToken = redisClient.wrap<[UserRead], EncodedToken>(
-    _createToken,
-    createTokenKeygen,
-    env.JWT_EXPIRATION
-);
-
-export const verifyToken = (token: string): DecodedUserToken => {
-    const decoded = jwt.verify(token, env.SECRET_KEY);
-    if (typeof decoded === "string") {
-        throw new ApiError(HttpStatus.BAD_REQUEST, "Invalid token payload");
-    }
-    return decoded as DecodedUserToken;
-};
-
 export const getUserFromToken = async (token: string): Promise<UserRead> => {
-    const payload = verifyToken(token);
+    const payload = decodeToken(token);
     const user = await crudUser.get(payload.userId);
     if (!user) {
         throw new ApiError(HttpStatus.NOT_FOUND, "User not found");
