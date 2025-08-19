b5c060a72ab3820e92757f8f31d31d74062c03b2
Slim Beji <mslimbeji@gmail.com>
2025-08-19 14:54:11 +0100

    debug the limit_json middleware

--- backend-fastapi/api/middlewares/json.py
+++ backend-fastapi/api/middlewares/json.py
@@ -2,30 +2,36 @@ from http import HTTPStatus
 
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from starlette.types import ASGIApp, Receive, Scope, Send
 
 from config import settings
-from types_ import AsgiMiddleware
+from types_ import CallNext
 
 
-class LimitJSONSizeMiddleware(AsgiMiddleware):
-    def __init__(self, app: ASGIApp, max_size: int = settings.JSON_MAX_SIZE):
-        self.app = app
-        self.max_size = max_size
+class RequestTooLarge(Exception):
+    pass
 
-    async def __call__(self, scope: Scope, receive: Receive, send: Send):
-        if scope["type"] == "http":
-            request = Request(scope, receive=receive)
-            if request.method in ("POST", "PUT", "PATCH"):
-                content_type = request.headers.get("content-type", "")
-                if content_type.startswith("application/json"):
-                    body = await request.body()
-                    if len(body) > self.max_size * 1024:
-                        response = JSONResponse(
-                            {"detail": "Request body too large"},
-                            status_code=HTTPStatus.REQUEST_ENTITY_TOO_LARGE,
-                        )
-                        await response(scope, receive, send)
-                        return
 
-        await self.app(scope, receive, send)
+async def limit_json_size(request: Request, call_next: CallNext):
+    if request.method in ("POST", "PUT", "PATCH"):
+        content_type = request.headers.get("content-type", "")
+        if content_type.startswith("application/json"):
+            received = 0
+            original_receive = request._receive
+
+            async def limited_receive():
+                nonlocal received
+                message = await original_receive()
+                received += len(message.get("body", b""))
+                if received > settings.JSON_MAX_SIZE:
+                    raise RequestTooLarge
+                return message
+
+            request._receive = limited_receive
+
+    try:
+        return await call_next(request)
+    except RequestTooLarge:
+        return JSONResponse(
+            {"detail": "Request body too large"},
+            status_code=HTTPStatus.REQUEST_ENTITY_TOO_LARGE,
+        )
