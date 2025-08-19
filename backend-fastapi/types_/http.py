cc333316912107f0d1636820cdce7351b672e4b3
Slim Beji <mslimbeji@gmail.com>
2025-08-19 11:17:32 +0100

    fix mypy linting

--- backend-fastapi/types_/http.py
+++ backend-fastapi/types_/http.py
@@ -56,10 +56,14 @@ class FileToUpload:
                 raise ValueError(f"File too large (> {cls.MAX_SIZE} MB)")
             return file
 
-        buffer = file.file.read()
+        upload_file = cast(UploadFile, file)
+        buffer = upload_file.file.read()
+        name = cast(str, upload_file.filename)
+        mimetype = cast(str, upload_file.content_type)
+
         if len(buffer) > cls.MAX_SIZE * 1024 * 1024:
             raise ValueError(f"File too large (> {cls.MAX_SIZE} MB)")
-        return cls(cast(str, file.filename), cast(str, file.content_type), buffer)
+        return cls(name, mimetype, buffer)
 
     @classmethod
     def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: Any):
@@ -75,7 +79,7 @@ class FileToUpload:
 
     @classmethod
     def __class_getitem__(cls, size_mb: int) -> type[Self]:
-        class FileWithCustomLimit(cls):
+        class FileWithCustomLimit(cls):  # type: ignore
             MAX_SIZE = size_mb
 
         FileWithCustomLimit.__name__ = f"FileToUpload_{size_mb}MB"
