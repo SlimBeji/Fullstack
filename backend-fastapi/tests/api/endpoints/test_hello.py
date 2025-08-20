6e851567ae589c91ba165c08d4c50e293df0ec39
Slim Beji <mslimbeji@gmail.com>
2025-08-20 10:04:29 +0100

    avoid hard coding http status

--- backend-fastapi/tests/api/endpoints/test_hello.py
+++ backend-fastapi/tests/api/endpoints/test_hello.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import pytest
 from httpx import AsyncClient
 
@@ -6,5 +8,5 @@ from httpx import AsyncClient
 async def test_hello_world(client: AsyncClient):
     resp = await client.get("/api/hello-world")
     j = resp.json()
-    assert resp.status_code == 200
+    assert resp.status_code == HTTPStatus.OK
     assert j == dict(message="Hello World!")
