3aa21ab8ed56e2370c6c30a5c13488d37442b6d0
Slim Beji <mslimbeji@gmail.com>
2025-08-21 11:12:37 +0100

    add a seed_test_data helper to the sync module

--- backend-fastapi/lib/sync/sync.py
+++ backend-fastapi/lib/sync/sync.py
@@ -1,4 +1,5 @@
 from lib.clients.utils import close_dbs, connect_dbs
+from models.examples import dump_db, seed_db
 from worker.crons import close_crons
 from worker.tasks import close_workers
 
@@ -11,3 +12,10 @@ async def close_all() -> None:
     await close_dbs()
     close_workers()
     close_crons()
+
+
+async def seed_test_data() -> None:
+    await connect_dbs()
+    await dump_db()
+    await seed_db()
+    await close_dbs()
