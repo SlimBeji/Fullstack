28e9ef8a3377ec38565b95369950b5e12bc0fff0
Slim Beji <mslimbeji@gmail.com>
2025-08-21 18:24:24 +0100

    remove mongodb-memory-server

--- backend-express/src/lib/clients/mongo.ts
+++ backend-express/src/lib/clients/mongo.ts
@@ -1,62 +1,28 @@
-import { MongoMemoryReplSet } from "mongodb-memory-server";
 import mongoose, { Mongoose } from "mongoose";
 
 import { env } from "../../config";
-import { seedDb } from "../../models/examples";
-import { CollectionEnum } from "../../types";
 
 export class MongoClient {
-    private conn: Mongoose | null = null;
-    private replSetMock: MongoMemoryReplSet | null = null;
+    public conn: Mongoose | null = null;
     public uri: string = env.MONGO_URL;
-    public dbName: string = env.MONGO_DBNAME;
 
     public get isTest(): boolean {
         return !!process.env.JEST_WORKER_ID;
     }
 
-    private async prepareMock(): Promise<void> {
-        if (!this.replSetMock) {
-            this.replSetMock = await MongoMemoryReplSet.create({
-                replSet: { count: 1 },
-                instanceOpts: [{ storageEngine: "wiredTiger" }],
-            });
-            this.uri = this.replSetMock.getUri();
+    public get dbName(): string {
+        if (this.isTest) {
+            return env.MONGO_TEST_DBNAME;
         }
-    }
-
-    private async seedMock(): Promise<void> {
-        if (!this.conn) {
-            throw new Error(
-                "Cannot seed while the connection is not established"
-            );
-        }
-        const wasSeeded = await this.conn.connection
-            .db!.listCollections({ name: CollectionEnum.USERS })
-            .hasNext();
-
-        if (!wasSeeded) {
-            await seedDb();
-        }
+        return env.MONGO_DBNAME;
     }
 
     public async connect(): Promise<void> {
-        if (this.isTest) {
-            await this.prepareMock();
-        }
         this.conn = await mongoose.connect(this.uri, { dbName: this.dbName });
-        if (this.isTest) {
-            await this.seedMock();
-        }
     }
 
     public async close(): Promise<void> {
         await mongoose.disconnect();
-        if (this.isTest && !!this.replSetMock) {
-            await this.replSetMock.stop();
-            this.replSetMock = null;
-            this.uri = "";
-        }
     }
 
     public async createCollection(name: string): Promise<void> {
