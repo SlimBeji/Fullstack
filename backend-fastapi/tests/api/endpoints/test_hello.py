7adf1abdc7a8465d2f5143ad71bc026484098006
Slim Beji <mslimbeji@gmail.com>
2025-08-21 11:34:24 +0100

    finish hello_world router tests

--- backend-fastapi/tests/api/endpoints/test_hello.py
+++ backend-fastapi/tests/api/endpoints/test_hello.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 import pytest
 from httpx import AsyncClient
 
+from models.crud import crud_user
+
 
 @pytest.mark.asyncio
 async def test_hello_world(client: AsyncClient):
@@ -10,3 +12,23 @@ async def test_hello_world(client: AsyncClient):
     j = resp.json()
     assert resp.status_code == HTTPStatus.OK
     assert j == dict(message="Hello World!")
+
+
+@pytest.mark.asyncio
+async def test_hello_user(client: AsyncClient):
+    token = await crud_user.get_bearer("mslimbeji@gmail.com")
+    headers = dict(Authorization=token)
+    resp = await client.get("/api/hello-world/user", headers=headers)
+    j = resp.json()
+    assert resp.status_code == HTTPStatus.OK
+    assert j == dict(message="Hello Slim Beji!")
+
+
+@pytest.mark.asyncio
+async def test_hello_admin(client: AsyncClient):
+    token = await crud_user.get_bearer("mslimbeji@gmail.com")
+    headers = dict(Authorization=token)
+    resp = await client.get("/api/hello-world/admin", headers=headers)
+    j = resp.json()
+    assert resp.status_code == HTTPStatus.OK
+    assert j == dict(message="Hello Admin Slim Beji!")
