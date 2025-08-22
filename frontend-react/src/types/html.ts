1f4e6a5713ebc776ac69fcd98f66b26c94519a4d
Slim Beji <mslimbeji@gmail.com>
2025-08-22 09:17:55 +0100

    split the react types module

---
+++ frontend-react/src/types/html.ts
@@ -0,0 +1,5 @@
+import { FormEvent } from "react";
+
+export type ButtonType = "button" | "submit" | "reset" | undefined;
+
+export type FormSubmitHandler = (e: FormEvent) => void;
--- frontend-react/src/types/index.ts
+++ frontend-react/src/types/index.ts
@@ -1,52 +1,5 @@
-import { FormEvent } from "react";
-
-export type HttpMethods = "get" | "post" | "put" | "delete";
-
-export type HeaderContent = "application/json" | "multipart/form-data";
-
-export type ButtonType = "button" | "submit" | "reset" | undefined;
-
-export type FormSubmitHandler = (e: FormEvent) => void;
-
-export interface SigninResponse {
-    access_token: string;
-    token_type: "bearer";
-    userId: string;
-    email: string;
-    expires_in: number;
-}
-
-export interface EncodedUserToken {
-    access_token: string;
-    token_type: "bearer";
-    userId: string;
-    email: string;
-    expiresAt: number;
-}
-
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    imageUrl?: string;
-    places: string[];
-}
-
-export interface Location {
-    lat: number;
-    lng: number;
-}
-
-export interface Place {
-    id: string;
-    title: string;
-    description: string;
-    imageUrl?: string;
-    address: string;
-    location: Location;
-    creatorId: string;
-}
-
-export enum LocalStorageKeys {
-    userData = "userData",
-}
+export * from "./auth";
+export * from "./enums";
+export * from "./html";
+export * from "./http";
+export * from "./models";
