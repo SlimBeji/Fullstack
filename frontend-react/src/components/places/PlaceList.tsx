f6ab9b14ff0811b849a77f79af4f265dd4367e26
Slim Beji <mslimbeji@gmail.com>
2025-08-27 16:38:31 +0100

    deprecate PlaceList.css

--- frontend-react/src/components/places/PlaceList.tsx
+++ frontend-react/src/components/places/PlaceList.tsx
@@ -1,5 +1,3 @@
-import "./PlaceList.css";
-
 import { Place } from "../../types";
 import PlaceItem from "./PlaceItem";
 
@@ -10,7 +8,7 @@ interface PlaceListProps {
 
 const PlaceList: React.FC<PlaceListProps> = ({ items, onDelete }) => {
     return (
-        <ul className="place-list">
+        <ul className="list-none m-4 mx-auto p-0 w-[90%] max-w-[40rem]">
             {items.map((p: Place) => {
                 return <PlaceItem key={p.id} place={p} onDelete={onDelete} />;
             })}
