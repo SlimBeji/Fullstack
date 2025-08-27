d79d824b5bf28f43b396d08ca202996a9f64483a
Slim Beji <mslimbeji@gmail.com>
2025-08-27 17:13:44 +0100

    deprecate MainHeader.css

--- frontend-react/src/components/navigation/MainHeader.tsx
+++ frontend-react/src/components/navigation/MainHeader.tsx
@@ -1,5 +1,3 @@
-import "./MainHeader.css";
-
 import { ReactNode } from "react";
 
 interface MainHeaderProps {
@@ -7,7 +5,11 @@ interface MainHeaderProps {
 }
 
 const MainHeader: React.FC<MainHeaderProps> = ({ children }) => {
-    return <header className="main-header">{children}</header>;
+    return (
+        <header className="w-full h-16 flex items-center fixed top-0 left-0 bg-[#ff0055] shadow-md px-4 z-50 md:justify-between">
+            {children}
+        </header>
+    );
 };
 
 export default MainHeader;
