4ea4dcf42292de8776ec2b30c617140107152b8a
Slim Beji <mslimbeji@gmail.com>
2025-08-27 16:34:17 +0100

    remove LoadingSpinner.css

--- frontend-react/src/components/ui/LoadingSpinner.tsx
+++ frontend-react/src/components/ui/LoadingSpinner.tsx
@@ -1,5 +1,3 @@
-import "./LoadingSpinner.css";
-
 import React from "react";
 
 interface LoadingSpinner {
@@ -7,9 +5,15 @@ interface LoadingSpinner {
 }
 
 const LoadingSpinner: React.FC<LoadingSpinner> = ({ asOverlay }) => {
+    const wrapperClasses = asOverlay
+        ? "absolute inset-0 bg-white/90 flex justify-center items-center"
+        : "";
+    const containerClasses =
+        "w-12 h-12 border-4 border-[#510077] border-t-transparent rounded-full animate-spin";
+
     return (
-        <div className={`${asOverlay && "loading-spinner__overlay"}`}>
-            <div className="lds-dual-ring"></div>
+        <div className={wrapperClasses}>
+            <div className={containerClasses}></div>
         </div>
     );
 };
