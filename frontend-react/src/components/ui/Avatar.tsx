e2f9ddedd403bb45c5a06441fd33d9d91e02e0fe
Slim Beji <mslimbeji@gmail.com>
2025-09-03 16:47:52 +0100

    refactor Avatar styling

--- frontend-react/src/components/ui/Avatar.tsx
+++ frontend-react/src/components/ui/Avatar.tsx
@@ -1,19 +1,12 @@
 interface AvatarProps {
-    className?: string;
     imageUrl: string;
     alt: string;
 }
 
 const Avatar: React.FC<AvatarProps> = (props) => {
     return (
-        <div
-            className={`flex justify-center items-center rounded-full overflow-hidden w-16 h-16 ${props.className || ""}`}
-        >
-            <img
-                src={props.imageUrl}
-                alt={props.alt}
-                className="w-full h-full object-cover"
-            />
+        <div className="avatar-container">
+            <img src={props.imageUrl} alt={props.alt} />
         </div>
     );
 };
