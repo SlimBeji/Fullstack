d11f4d77a07bbca43def61b1cda28def066c70dd
Slim Beji <mslimbeji@gmail.com>
2025-09-03 19:28:36 +0100

    refactor Modal styling

--- frontend-react/src/components/ui/Modal.tsx
+++ frontend-react/src/components/ui/Modal.tsx
@@ -23,27 +23,17 @@ const ModalOverlay: React.FC<ModalOverlayProps> = ({
     header,
     footer,
     onSubmit,
-    className,
     style,
-    headerClass,
-    contentClass,
-    footerClass,
     ref,
 }) => {
     const content = (
-        <div
-            ref={ref}
-            className={`fixed top-[22vh] left-[10%] w-[80%] bg-white rounded-lg shadow-lg z-50 md:left-1/2 md:w-[40rem] md:-translate-x-1/2 ${className}`}
-            style={style}
-        >
-            <header
-                className={`w-full p-4 bg-indigo-900 text-white ${headerClass}`}
-            >
-                <h2 className="m-2 text-xl font-semibold">{header}</h2>
+        <div ref={ref} className="modal-container" style={style}>
+            <header>
+                <h2>{header}</h2>
             </header>
             <form onSubmit={onSubmit ? onSubmit : (e) => e.preventDefault()}>
-                <div className={`p-4 ${contentClass}`}>{children}</div>
-                <footer className={`p-4 ${footerClass}`}>{footer}</footer>
+                <div>{children}</div>
+                <footer>{footer}</footer>
             </form>
         </div>
     );
@@ -63,12 +53,12 @@ const Modal: React.FC<ModalProps> = ({ show, onCancel, ...overlayProps }) => {
             <Transition
                 as={Fragment}
                 show={show}
-                enter="transition-all duration-300 ease-out transform"
-                enterFrom="opacity-0 -translate-y-20"
-                enterTo="opacity-100 translate-y-0"
-                leave="transition-all transform"
-                leaveFrom="opacity-100 translate-y-0"
-                leaveTo="opacity-0 -translate-y-[100vh] duration-500 ease-in"
+                enter="modal effect"
+                enterFrom="modal a"
+                enterTo="modal b"
+                leave="modal effect"
+                leaveFrom="modal b"
+                leaveTo="modal a"
             >
                 <ModalOverlay ref={nodeRef} {...overlayProps}></ModalOverlay>
             </Transition>
