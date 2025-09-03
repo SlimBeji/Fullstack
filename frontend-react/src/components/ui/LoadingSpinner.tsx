8619d55b57f4e53abd5679b04b6f74e82d1596f0
Slim Beji <mslimbeji@gmail.com>
2025-09-03 17:02:51 +0100

    refactor Spinner styling

--- frontend-react/src/components/ui/LoadingSpinner.tsx
+++ frontend-react/src/components/ui/LoadingSpinner.tsx
@@ -6,14 +6,8 @@ interface LoadingSpinner {
 
 const LoadingSpinner: React.FC<LoadingSpinner> = ({ asOverlay }) => {
     return (
-        <div
-            className={
-                asOverlay
-                    ? "absolute inset-0 bg-white/90 flex justify-center items-center"
-                    : ""
-            }
-        >
-            <div className="w-12 h-12 border-4 border-[#510077] border-t-transparent rounded-full animate-spin"></div>
+        <div className={`spinner-container ${asOverlay ? "overlay" : ""}`}>
+            <div></div>
         </div>
     );
 };
