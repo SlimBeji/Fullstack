4f04aa3acd16136f663511de64c328f0906a84b7
Slim Beji <mslimbeji@gmail.com>
2025-09-09 22:56:09 +0100

    remove the logo.svg

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -6,17 +6,8 @@ console.log(import.meta.env.VITE_BACKEND_URL);
 
 <template>
     <header>
-        <img
-            alt="Vue logo"
-            class="logo"
-            src="@/assets/logo.svg"
-            width="125"
-            height="125"
-        />
-
         <div class="wrapper">
             <HelloWorld msg="You did it!" />
-
             <nav>
                 <RouterLink to="/auth">Auth</RouterLink>
                 <RouterLink to="/">Users</RouterLink>
