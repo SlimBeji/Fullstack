a261083b3007ca46f7094763afc24ae82790d2a6
Slim Beji <mslimbeji@gmail.com>
2025-09-10 15:19:04 +0100

    create lib/utils module

---
+++ frontend-vue/src/lib/utils/axios.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosInstance } from "axios";
+
+import { HeaderContent } from "../../types";
+import { getToken } from "./storage";
+
+export const getClient = (
+    contentType: HeaderContent = "application/json"
+): AxiosInstance => {
+    const token = getToken();
+    const headers = {
+        "Content-Type": contentType,
+        ...(token && { Authorization: token }),
+    };
+
+    return axios.create({
+        baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
+        headers,
+    });
+};
+
+export const backendApi = getClient();
