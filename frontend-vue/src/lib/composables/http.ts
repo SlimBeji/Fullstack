254b0ec0d9f28d93d5825c809f8327b4ff3f2484
Slim Beji <mslimbeji@gmail.com>
2025-09-10 20:22:11 +0100

    create a useHttp composable

---
+++ frontend-vue/src/lib/composables/http.ts
@@ -0,0 +1,124 @@
+import { AxiosError, AxiosResponse, HttpStatusCode } from "axios";
+import { ref } from "vue";
+
+import { HeaderContent, HttpMethods } from "@/types";
+
+import { getClient } from "../utils";
+
+const TOKEN_EXPIRED = "Token expired";
+
+interface State {
+    loading: boolean;
+    statusCode?: number;
+    json?: Record<string, any>;
+    error?: {
+        tokenExpired?: boolean;
+        message: string;
+        response?: AxiosResponse;
+    };
+}
+
+interface useHttpOptions {
+    ignoreNotFound?: boolean;
+}
+
+export const useHttp = (options: useHttpOptions = {}) => {
+    const emptyState: State = { loading: false };
+
+    const abortControllerRef = ref<AbortController | null>(null);
+    const httpData = ref<State>(emptyState);
+
+    const resetState = () => {
+        httpData.value = { loading: false };
+    };
+
+    const handleSuccess = (resp: AxiosResponse) => {
+        httpData.value = {
+            loading: false,
+            statusCode: resp.status,
+            json: resp.data,
+        };
+    };
+
+    const handleError = (err: AxiosError) => {
+        const statusCode = err.status;
+        const response = err.response;
+        const data = response?.data as {
+            message?: string;
+        };
+        const message = data.message || "Something went wrong!";
+        const tokenExpired = message === TOKEN_EXPIRED;
+
+        httpData.value = {
+            loading: false,
+            statusCode,
+            error: { tokenExpired, message, response },
+        };
+    };
+
+    const sendRequest = async (
+        url: string,
+        method: HttpMethods,
+        data?: object,
+        tokenRequired: boolean = true
+    ): Promise<AxiosResponse> => {
+        // Create abort controller
+        abortControllerRef.value?.abort();
+        abortControllerRef.value = new AbortController();
+
+        // Prepare the web client
+        let contentType: HeaderContent = "application/json";
+        if (data instanceof FormData) {
+            contentType = "multipart/form-data";
+        }
+        const webClient = getClient(contentType);
+
+        // Check the Token
+        const token = webClient.defaults.headers.Authorization;
+        if (!token && tokenRequired) {
+            abortControllerRef.value = null;
+            httpData.value = {
+                loading: false,
+                error: {
+                    tokenExpired: true,
+                    message: TOKEN_EXPIRED,
+                },
+            };
+            // We raise the same error as if we actually send
+            // a request and got a token expired response
+            throw new AxiosError(TOKEN_EXPIRED);
+        }
+
+        try {
+            httpData.value = { loading: true };
+            const resp = await webClient[method](url, data, {
+                signal: abortControllerRef.value.signal,
+            });
+            handleSuccess(resp);
+            return resp;
+        } catch (err) {
+            // Handle error
+            if (err instanceof AxiosError) {
+                if (
+                    err.response?.status === HttpStatusCode.NotFound &&
+                    options.ignoreNotFound
+                ) {
+                    handleSuccess(err.response);
+                    return err.response;
+                } else {
+                    handleError(err);
+                }
+            }
+
+            throw err;
+        } finally {
+            abortControllerRef.value = null;
+        }
+    };
+
+    return {
+        httpData,
+        sendRequest,
+        resetState,
+    };
+};
