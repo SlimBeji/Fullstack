08902d22d978b0bde9f74601458c62148854e8fe
Slim Beji <mslimbeji@gmail.com>
2025-09-15 22:00:52 +0100

    refactor the express mongo type module

--- backend-express/src/types/mongo.ts
+++ backend-express/src/types/mongo.ts
@@ -2,49 +2,49 @@ import { FilterQuery, ProjectionType } from "mongoose";
 
 import { PaginationData } from "./http";
 
-export type SortData = { [key: string]: 1 | -1 };
+//// Public Types for searching data
 
-export const MongoOperationsMapping = {
-    eq: "$eq",
-    ne: "$ne",
-    gt: "$gt",
-    gte: "$gte",
-    lt: "$lt",
-    lte: "$lte",
-    in: "$in",
-    nin: "$nin",
-    regex: "$regex",
-    text: "$text",
-} as const;
-
-export type FilterOperation = keyof typeof MongoOperationsMapping;
-
-export type QueryFilter = { op: FilterOperation; val: string[] };
+export type FilterOperation =
+    | "eq"
+    | "ne"
+    | "gt"
+    | "gte"
+    | "lt"
+    | "lte"
+    | "in"
+    | "nin"
+    | "regex"
+    | "text"
+    | "exists";
 
 export type Filter = { op: FilterOperation; val: any };
 
-interface BaseFilterBody {
+export type FindQueryFilters<T extends string> = { [K in T]?: Filter };
+
+export interface FindQuery<
+    Selectables extends string,
+    Sortables extends string,
+    Searchables extends string,
+> {
+    page?: number;
+    size?: number;
+    sort?: Sortables[];
+    fields?: Selectables[];
+    filters?: FindQueryFilters<Searchables>;
+}
+
+export type RawFindQuery = {
     page?: number;
     size?: number;
     sort?: string[];
     fields?: string[];
-}
+} & Record<string, { op: FilterOperation; val: string[] }[]>;
 
-export type RawFindQuery = BaseFilterBody & Record<string, QueryFilter[]>;
+//// Internal Types for building Mongo queries
 
-export type FindQueryFilters<S = unknown> = S extends object
-    ? Partial<Record<keyof S, Filter[]>>
-    : Record<string, Filter[]>;
+export type SortData = { [key: string]: 1 | -1 };
 
-export interface FindQuery<S = unknown> {
-    page?: number;
-    size?: number;
-    sort?: string[];
-    fields?: string[];
-    filters?: FindQueryFilters<S>;
-}
-
-export type MongoFieldFilters = { [key in `$${FilterOperation}`]?: any };
+export type MongoFieldFilters = { [key: string]: any };
 
 export type MongoFieldsFilters = { [key: string]: MongoFieldFilters };
 
