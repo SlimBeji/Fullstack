13c56ea11cd9aaae7e3b24c0dc1a45a3f82cb42c
Slim Beji <mslimbeji@gmail.com>
2025-09-16 19:24:29 +0100

    create a main navigation component

---
+++ frontend-vue/src/components/navigation/MainNavigation.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/auth">Auth</RouterLink>
+                <RouterLink to="/">Users</RouterLink>
+                <RouterLink to="/places/new">New Place</RouterLink>
+                <RouterLink to="/places/p123">Edit Place</RouterLink>
+                <RouterLink to="/u123/places">User Places</RouterLink>
+            </nav>
+        </div>
+    </header>
+</template>
