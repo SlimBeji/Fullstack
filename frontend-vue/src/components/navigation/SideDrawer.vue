bc8d1b63efeffa2a69d5082fe106bdad9228ac98
Slim Beji <mslimbeji@gmail.com>
2025-09-17 22:18:53 +0100

    create a SideDrawer component

---
+++ frontend-vue/src/components/navigation/SideDrawer.vue
@@ -0,0 +1,42 @@
+<template>
+    <Teleport to="#drawer-hook">
+        <Transition name="sidedrawer-transition" appear>
+            <aside v-if="show" class="sidedrawer" @click="onClick">
+                <slot />
+            </aside>
+        </Transition>
+    </Teleport>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    show: boolean;
+    onClick: () => void;
+}>();
+</script>
+
+<style lang="css">
+@reference "../../main.css";
+
+@layer components {
+    aside.sidedrawer {
+        @apply fixed z-40 top-0 left-0 h-screen w-[70%];
+        @apply bg-surface shadow-md;
+    }
+
+    .sidedrawer-transition-enter-active,
+    .sidedrawer-transition-leave-active {
+        @apply transform transition duration-200;
+    }
+
+    .sidedrawer-transition-enter-from,
+    .sidedrawer-transition-leave-to {
+        @apply -translate-x-full opacity-0;
+    }
+
+    .sidedrawer-transition-enter-to,
+    .sidedrawer-transition-leave-from {
+        @apply -translate-x-0 opacity-100;
+    }
+}
+</style>
--- frontend-vue/src/components/navigation/index.ts
+++ frontend-vue/src/components/navigation/index.ts
@@ -1,2 +1,3 @@
 export { default as MainNavigation } from "./MainNavigation.vue";
 export { default as NavLinks } from "./NavLinks.vue";
+export { default as SideDrawer } from "./SideDrawer.vue";
