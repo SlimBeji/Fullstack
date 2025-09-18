e558374085688706feb2c26729569e33208ec7b6
Slim Beji <mslimbeji@gmail.com>
2025-09-18 16:41:54 +0100

    create a loading spinner in vue

---
+++ frontend-vue/src/components/ui/LoadingSpinner.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="spinner-container" :class="{ overlay: props.asOverlay }">
+        <div></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+// Props definition
+const props = defineProps<{
+    asOverlay?: boolean;
+}>();
+</script>
+
+<style lang="css">
+@reference "../../main.css";
+@layer components {
+    .spinner-container.overlay {
+        @apply absolute inset-0 bg-surface/90 flex justify-center items-center;
+    }
+
+    .spinner-container > div {
+        @apply w-12 h-12 rounded-full animate-spin;
+        @apply border-4 border-secondary border-t-transparent;
+    }
+}
+</style>
--- frontend-vue/src/components/ui/index.ts
+++ frontend-vue/src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as Backdrop } from "./Backdrop.vue";
+export { default as LoadingSpinner } from "./LoadingSpinner.vue";
