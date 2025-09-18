3382fddedb4dde2c3100aa4df38542c501cdef2a
Slim Beji <mslimbeji@gmail.com>
2025-09-18 19:11:34 +0100

    create ErrorModal compoennt in Vue

---
+++ frontend-vue/src/components/ui/ErrorModal.vue
@@ -0,0 +1,25 @@
+<template>
+    <Modal
+        :onCancel="props.onClear"
+        :show="!!props.error"
+        :header="props.header || `An Error Occured!`"
+    >
+        <p>{{ props.error }}</p>
+        <template #footer>
+            <Button :onClick="props.onClear">Okay</Button>
+        </template>
+    </Modal>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/form";
+
+import Modal from "./Modal.vue";
+
+// Props
+const props = defineProps<{
+    error: string;
+    header?: string;
+    onClear: () => void;
+}>();
+</script>
--- frontend-vue/src/components/ui/index.ts
+++ frontend-vue/src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as Backdrop } from "./Backdrop.vue";
+export { default as ErrorModal } from "./ErrorModal.vue";
 export { default as LoadingSpinner } from "./LoadingSpinner.vue";
 export { default as Modal } from "./Modal.vue";
