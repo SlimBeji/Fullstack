418f393b0b0767c3d68564cbc515043e6e723374
Slim Beji <mslimbeji@gmail.com>
2025-09-20 20:05:29 +0100

    create PlacesList vue component

---
+++ frontend-vue/src/components/places/PlacesList.vue
@@ -0,0 +1,63 @@
+<template>
+    <ul class="places-list">
+        <template v-if="props.items.length > 0">
+            <PlaceItem
+                v-for="place in props.items"
+                @delete="emit('delete')"
+                :key="place.id"
+                :place="place"
+            />
+        </template>
+        <div v-else-if="props.sameAuthenticatedUser" class="no-places">
+            <div class="card">
+                <h2>No places found. Maybe create one?</h2>
+                <Button color="secondary" to="/places/new"> Share </Button>
+            </div>
+        </div>
+        <div v-else class="no-places">
+            <div class="card">
+                <h2>This user has not created places yet</h2>
+            </div>
+        </div>
+    </ul>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/form";
+import { Place } from "@/types";
+
+import PlaceItem from "./PlaceItem.vue";
+
+// Props
+const props = defineProps<{
+    sameAuthenticatedUser?: boolean;
+    items: Place[];
+}>();
+
+// Events
+const emit = defineEmits<{
+    (e: "delete"): void;
+}>();
+</script>
+
+<style lang="css">
+@reference "../../main.css";
+
+@layer components {
+    .places-list {
+        @apply w-[90%] max-w-[40rem] my-4 mx-auto p-0 list-none;
+    }
+
+    .no-places {
+        @apply mx-auto w-100 max-w-[90%];
+    }
+
+    .no-places .card {
+        @apply p-5 text-center;
+    }
+
+    .no-places .card h2 {
+        @apply my-5;
+    }
+}
+</style>
--- frontend-vue/src/components/places/index.ts
+++ frontend-vue/src/components/places/index.ts
@@ -1,1 +1,2 @@
 export { default as PlaceItem } from "./PlaceItem.vue";
+export { default as PlacesList } from "./PlacesList.vue";
