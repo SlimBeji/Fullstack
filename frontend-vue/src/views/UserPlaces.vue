ff642dbccf43b0f41f25ea716264a0ef95370ca6
Slim Beji <mslimbeji@gmail.com>
2025-09-20 20:30:08 +0100

    finish UserPlaces vue view

--- frontend-vue/src/views/UserPlaces.vue
+++ frontend-vue/src/views/UserPlaces.vue
@@ -1,12 +1,59 @@
+<template>
+    <HttpError
+        v-if="httpData.error?.message"
+        @close="clear"
+        :error="httpData.error"
+    />
+    <LoadingSpinner v-if="httpData.loading" as-overlay />
+    <PlacesList
+        @delete="deleteHandler"
+        :items="places"
+        :same-authenticated-user="isUserOwned"
+    />
+</template>
+
 <script setup lang="ts">
+import { HttpStatusCode } from "axios";
+import { computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 
+import { PlacesList } from "@/components/places";
+import { HttpError, LoadingSpinner } from "@/components/ui";
+import { useHttp } from "@/lib";
+import { useAuthStore } from "@/stores";
+import { Place } from "@/types";
+
+// Setup
+const authStore = useAuthStore();
 const route = useRoute();
 const userId = route.params.userId as string;
-</script>
+const { httpData, sendRequest, clear } = useHttp({ ignoreNotFound: true });
 
-<template>
-    <div>
-        <h1>This is the page for user {{ userId }} places</h1>
-    </div>
-</template>
+// Events
+onMounted(() => {
+    fetchPlaces(userId);
+});
+
+// Computed
+const isUserOwned = computed(() => {
+    return userId === authStore.userId;
+});
+
+const places = computed(() => {
+    let result = (httpData.value.json?.data as Place[]) || [];
+    if (httpData.value.statusCode === HttpStatusCode.NotFound) result = [];
+    return result;
+});
+
+// Handlers
+const fetchPlaces = (userId: string | undefined) => {
+    if (!userId) return;
+    sendRequest(`/places/?creatorId=${userId}`, "get").catch((err) =>
+        console.log(err)
+    );
+};
+
+const deleteHandler = async () => {
+    fetchPlaces(userId);
+};
+</script>
