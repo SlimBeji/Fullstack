7a60a5608717577f64874bfb940838c7b4380085
Slim Beji <mslimbeji@gmail.com>
2025-09-20 23:27:01 +0100

    refactor PlaceUpdateForm react compoent

--- frontend-react/src/components/places/PlaceUpdateForm.tsx
+++ frontend-react/src/components/places/PlaceUpdateForm.tsx
@@ -70,83 +70,83 @@ const PlaceUpdateForm: React.FC<PlaceUpdateFormProps> = ({ placeId }) => {
     const descriptionValidators = useMemo(() => [minLengthValidator(10)], []);
     const coordinateValidators = useMemo(() => [numericValidator()], []);
 
-    if (data.error) {
+    if (data.error?.message) {
         return <HttpError error={data.error} onClear={clearError} />;
     }
 
-    if (!data.loading && !data.error) {
-        return (
-            <form className="place-update" onSubmit={submitHandler}>
-                <Input
-                    id="title"
-                    element="input"
-                    type="text"
-                    onInput={inputHandlers.title}
-                    label="Title"
-                    validators={titleValidators}
-                    errorText="Please enter a valid Title"
-                    value={state.inputs.title.val}
-                    isValid={state.inputs.title.isValid}
-                />
-                <Input
-                    id="address"
-                    element="input"
-                    type="text"
-                    onInput={inputHandlers.address}
-                    label="Address"
-                    validators={addressValidators}
-                    errorText="Please enter a valid Address"
-                    value={state.inputs.address.val}
-                    isValid={state.inputs.address.isValid}
-                />
-                <Input
-                    id="description"
-                    element="textarea"
-                    onInput={inputHandlers.description}
-                    label="Description"
-                    validators={descriptionValidators}
-                    errorText="Please enter a valid Description"
-                    value={state.inputs.description.val}
-                    isValid={state.inputs.description.isValid}
-                />
-                <Input
-                    id={"latitude"}
-                    width="1/2"
-                    padding="pr-2"
-                    element="input"
-                    onInput={inputHandlers.lat}
-                    label="Latitude"
-                    validators={coordinateValidators}
-                    errorText="Please enter a valid Latitude"
-                    value={state.inputs.lat.val}
-                    isValid={state.inputs.lat.isValid}
-                />
-                <Input
-                    id={"longitude"}
-                    width="1/2"
-                    padding="pl-2"
-                    element="input"
-                    onInput={inputHandlers.lng}
-                    label="Longitude"
-                    validators={coordinateValidators}
-                    errorText="Please enter a valid Longitude"
-                    value={state.inputs.lng.val}
-                    isValid={state.inputs.lng.isValid}
-                />
-                <div className="buttons">
-                    <Button
-                        type="submit"
-                        color="secondary"
-                        disabled={!state.isValid}
-                    >
-                        Edit Place
-                    </Button>
-                </div>
-            </form>
-        );
+    if (data.loading) {
+        return <LoadingSpinner asOverlay />;
     }
 
-    return <LoadingSpinner asOverlay />;
+    return (
+        <form className="place-update" onSubmit={submitHandler}>
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                onInput={inputHandlers.title}
+                label="Title"
+                validators={titleValidators}
+                errorText="Please enter a valid Title"
+                value={state.inputs.title.val}
+                isValid={state.inputs.title.isValid}
+            />
+            <Input
+                id="address"
+                element="input"
+                type="text"
+                onInput={inputHandlers.address}
+                label="Address"
+                validators={addressValidators}
+                errorText="Please enter a valid Address"
+                value={state.inputs.address.val}
+                isValid={state.inputs.address.isValid}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                onInput={inputHandlers.description}
+                label="Description"
+                validators={descriptionValidators}
+                errorText="Please enter a valid Description"
+                value={state.inputs.description.val}
+                isValid={state.inputs.description.isValid}
+            />
+            <Input
+                id="latitude"
+                width="1/2"
+                padding="pr-2"
+                element="input"
+                onInput={inputHandlers.lat}
+                label="Latitude"
+                validators={coordinateValidators}
+                errorText="Please enter a valid Latitude"
+                value={state.inputs.lat.val}
+                isValid={state.inputs.lat.isValid}
+            />
+            <Input
+                id="longitude"
+                width="1/2"
+                padding="pl-2"
+                element="input"
+                onInput={inputHandlers.lng}
+                label="Longitude"
+                validators={coordinateValidators}
+                errorText="Please enter a valid Longitude"
+                value={state.inputs.lng.val}
+                isValid={state.inputs.lng.isValid}
+            />
+            <div className="buttons">
+                <Button
+                    type="submit"
+                    color="secondary"
+                    disabled={!state.isValid}
+                >
+                    Edit Place
+                </Button>
+            </div>
+        </form>
+    );
 };
 
 export default PlaceUpdateForm;
