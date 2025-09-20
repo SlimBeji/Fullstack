6f583284c70938791e98fb0b360c4108dee9b6ac
Slim Beji <mslimbeji@gmail.com>
2025-09-20 13:29:32 +0100

    updating the Vue Users page

--- frontend-vue/src/views/Users.vue
+++ frontend-vue/src/views/Users.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>
-        <h1>This is the users page</h1>
-    </div>
+    <HttpError
+        v-if="httpData.error?.message"
+        @close="clear"
+        :error="httpData.error"
+        header="Could not fetch users!"
+    />
+    <LoadingSpinner v-if="httpData.loading" as-overlay />
+    <UsersList v-if="httpData.json?.data" :items="items" />
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { HttpError, LoadingSpinner } from "@/components/ui";
+import { UsersList } from "@/components/user";
+import { useHttp } from "@/lib";
+import { User } from "@/types";
+
+const { httpData, sendRequest, clear } = useHttp();
+sendRequest("/users/", "get");
+
+// Computed
+const items = computed((): User[] => {
+    if (httpData.value.json?.data) {
+        return httpData.value.json?.data as User[];
+    }
+    return [];
+});
+</script>
