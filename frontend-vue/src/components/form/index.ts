ae3a93a7ef2c2529dadeb87869363459d475d3c4
Slim Beji <mslimbeji@gmail.com>
2025-09-21 22:02:42 +0100

    Revert "rename ImageUpload to FileUpload"
    This reverts commit edfbde3e1756a325fe8631f5b8f4a64bf6ea94e5.

--- frontend-vue/src/components/form/index.ts
+++ frontend-vue/src/components/form/index.ts
@@ -1,3 +1,3 @@
 export { default as Button } from "./Button.vue";
-export { default as FileUpload } from "./FileUpload.vue";
+export { default as ImageUpload } from "./ImageUpload.vue";
 export { default as Input } from "./Input.vue";
