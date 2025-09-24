7e39e04a52562f40a3fb2354e6208c9db3c02cc1
Slim Beji <mslimbeji@gmail.com>
2025-09-24 10:23:45 +0100

    copy assets folder

--- frontend-svelte/src/App.svelte
+++ frontend-svelte/src/App.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
     import viteLogo from "/vite.svg";
-
-    import svelteLogo from "./assets/svelte.svg";
     import Counter from "./lib/Counter.svelte";
 </script>
 
@@ -9,9 +7,6 @@
     <div>
         <a href="https://vite.dev" target="_blank" rel="noreferrer">
             <img src={viteLogo} class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
         </a>
     </div>
     <h1>Vite + Svelte</h1>
@@ -42,9 +37,6 @@
     }
     .logo:hover {
         filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00aa);
     }
     .read-the-docs {
         color: #888;
