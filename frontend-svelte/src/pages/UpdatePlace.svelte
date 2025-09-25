13bbb5316dd22f8751813bb01d6eab4561c2e06a
Slim Beji <mslimbeji@gmail.com>
2025-09-25 19:14:31 +0100

    add examples on how to extract query parameters to UpdatePlace and UserPlaces pages

--- frontend-svelte/src/pages/UpdatePlace.svelte
+++ frontend-svelte/src/pages/UpdatePlace.svelte
@@ -1,1 +1,6 @@
-<h1>Update existing place</h1>
+<script lang="ts">
+const { route } = $props();
+const placeId: string = route.result.path.params.placeId;
+</script>
+
+<h1>Update existing place with id: {placeId}</h1>
--- frontend-svelte/src/pages/UserPlaces.svelte
+++ frontend-svelte/src/pages/UserPlaces.svelte
@@ -1,1 +1,6 @@
-<h1>List of a user places</h1>
+<script lang="ts">
+const { route } = $props();
+const userId: string = route.result.path.params.userId;
+</script>
+
+<h1>List of a user {userId} places</h1>
