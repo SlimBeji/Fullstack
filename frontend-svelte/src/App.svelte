57209d5eb091ef0cd94ae50fc10fbab1e0bb1d8d
Slim Beji <mslimbeji@gmail.com>
2025-09-25 19:17:45 +0100

    create empty MainNavigation component

--- frontend-svelte/src/App.svelte
+++ frontend-svelte/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+import { MainNavigation } from "@/components/navigation";
 import { AppRouter } from "@/router";
 </script>
 
+<MainNavigation />
 <main>
     <AppRouter />
 </main>
