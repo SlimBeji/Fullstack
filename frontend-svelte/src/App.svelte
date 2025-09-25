9f9631a12444de117207d9735a5209dfedbb6cab
Slim Beji <mslimbeji@gmail.com>
2025-09-25 10:27:19 +0100

    update App.svelte with example of useHttp

--- frontend-svelte/src/App.svelte
+++ frontend-svelte/src/App.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+    import { useHttp } from "@/lib";
     import { authStore } from "@/store";
     import type { SigninResponse } from "@/types";
 
+    const { httpData, sendRequest, clear } = useHttp();
+
+    async function load() {
+        await sendRequest(
+            "/api/hello-world",
+            "get",
+            undefined,
+            false
+        );
+    }
     const userId = authStore.userId;
     const login = () => {
         const resp: SigninResponse = {
@@ -22,6 +33,28 @@
     <p>
         Welcome User {$userId}
     </p>
-    <button onclick={login}>Login</button>
-    <button onclick={logout}>Logout</button>
+    <button class="test" onclick={login}>Login</button>
+    <button class="test" onclick={logout}>Logout</button>
+
+    <hr />
+    <button class="test" onclick={load}>Load</button>
+    <button class="test" onclick={clear}>Clear</button>
+
+    {#if $httpData.loading}
+        <p>Loading...</p>
+    {:else if $httpData.error}
+        <p>Error: {$httpData.error.message}</p>
+    {:else if $httpData.json}
+        <pre>{JSON.stringify($httpData.json, null, 2)}</pre>
+    {:else}
+        <p>Nothing to show</p>
+    {/if}
 </main>
+
+<style lang="css">
+@reference "./main.css";
+
+button {
+    @apply bg-red-500 border border-black;
+}
+</style>
