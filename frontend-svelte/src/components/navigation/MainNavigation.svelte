7ab398add8595e1a63cc99d55923837918a8457d
Slim Beji <mslimbeji@gmail.com>
2025-09-25 23:16:39 +0100

    add an example on how to work with Backdrop in MainNavigation component

--- frontend-svelte/src/components/navigation/MainNavigation.svelte
+++ frontend-svelte/src/components/navigation/MainNavigation.svelte
@@ -1,1 +1,15 @@
+<script lang="ts">
+import {Backdrop} from "@/components/ui";
+
+let showBackdrop = $state(true)
+
+function clickHandler() {
+    showBackdrop = false
+}
+</script>
+
+{#if showBackdrop }
+    <Backdrop onClick={clickHandler} />
+{/if}
+
 <h1>Main Navigation</h1>
