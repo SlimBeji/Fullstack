b1779bce57ae631960ebff2a230c1ab515d95358
Slim Beji <mslimbeji@gmail.com>
2025-09-27 17:37:17 +0100

    update fetching logic in UserPlaces

--- frontend-svelte/src/pages/UserPlaces.svelte
+++ frontend-svelte/src/pages/UserPlaces.svelte
@@ -1,6 +1,51 @@
 <script lang="ts">
+import { HttpStatusCode } from "axios";
+import { onMount } from "svelte";
+
+import { HttpError, LoadingSpinner } from "@/components/ui";
+import { useHttp } from "@/lib";
+import { authStore } from "@/store";
+import type { Place } from "@/types";
+
+// Init
 const { route } = $props();
 const userId: string = route.result.path.params.userId;
+const { httpData, sendRequest, clear } = useHttp({ ignoreNotFound: true });
+const loggedUserId = authStore.userId;
+
+// Events
+onMount(() => {
+    fetchPlaces(userId);
+});
+
+// Computed
+const isUserOwned = $derived<boolean>(userId === $loggedUserId);
+
+const places = $derived<Place[]>(
+    $httpData.statusCode === HttpStatusCode.NotFound
+        ? []
+        : $httpData.json?.data || []
+);
+
+// Handlers
+const fetchPlaces = (userId: string | undefined) => {
+    if (!userId) return;
+    sendRequest(`/places/?creatorId=${userId}`, "get").catch((err) =>
+        console.log(err)
+    );
+};
+
+const deleteHandler = async () => {
+    fetchPlaces(userId);
+};
 </script>
 
-<h1>List of a user {userId} places</h1>
+{#if $httpData.error?.message}
+    <HttpError onClose={clear} error={$httpData.error} />
+{/if}
+{#if $httpData.loading}
+    <LoadingSpinner asOverlay />
+{/if}
+<span>{isUserOwned}</span>
+<span>{deleteHandler}</span>
+<pre>{places}</pre>
