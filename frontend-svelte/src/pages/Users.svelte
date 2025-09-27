5dc7b4cb47f87aebd0b642e5c64574716f0e08ff
Slim Beji <mslimbeji@gmail.com>
2025-09-27 11:35:40 +0100

    fetching users in Users page

--- frontend-svelte/src/pages/Users.svelte
+++ frontend-svelte/src/pages/Users.svelte
@@ -1,1 +1,26 @@
-<h1>List of users</h1>
+<script lang="ts">
+import { HttpError, LoadingSpinner } from "@/components/ui";
+import { useHttp } from "@/lib";
+import type { User } from "@/types";
+
+// Init
+const { httpData, sendRequest, clear } = useHttp();
+sendRequest("/users/", "get");
+
+// Computed
+const items = $derived<User[]>($httpData.json?.data ?? []);
+</script>
+
+{#if $httpData.error?.message}
+    <HttpError
+        onClose={clear}
+        error={$httpData.error}
+        header="Could not fetch users!"
+    />
+{/if}
+{#if $httpData.loading}
+    <LoadingSpinner asOverlay />
+{/if}
+{#if $httpData.json?.data}
+    <pre>{items}</pre>
+{/if}
