f2b6e83a68de1ceecb9f864202fc22d2068a9790
Slim Beji <mslimbeji@gmail.com>
2025-09-29 19:13:35 +0100

    create base users endpoints

--- backend-gin/internal/api/routes/main.go
+++ backend-gin/internal/api/routes/main.go
@@ -5,4 +5,5 @@ import "github.com/gin-gonic/gin"
 func RegisterRoutes(r *gin.Engine) {
 	RegisterHelloWorld(r)
 	RegisterAuth(r)
+	RegisterUsers(r)
 }
---
+++ backend-gin/internal/api/routes/users.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"backend/internal/api/middlewares"
+	"backend/internal/models/schemas"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func createUser(c *gin.Context) {
+	user := c.MustGet("currentUser").(schemas.User)
+	c.JSON(http.StatusOK, user)
+}
+
+func getUser(c *gin.Context) {
+	fmt.Println(c.Param("userId"))
+	user := c.MustGet("currentUser").(schemas.User)
+	c.JSON(http.StatusOK, user)
+}
+
+func updateUser(c *gin.Context) {
+	fmt.Println(c.Param("userId"))
+	user := c.MustGet("currentUser").(schemas.User)
+	c.JSON(http.StatusOK, user)
+}
+
+func deleteUser(c *gin.Context) {
+	userId := c.Param("userId")
+	message := fmt.Sprintf("Deleted user %s", userId)
+	c.JSON(http.StatusOK, gin.H{"message": message})
+}
+
+func RegisterUsers(r *gin.Engine) {
+	router := r.Group(("/api/users"))
+	router.POST("", middlewares.Admin, createUser)
+	router.GET("/:userId", middlewares.Authenticated, getUser)
+	router.PUT("/:userId", middlewares.Authenticated, updateUser)
+	router.DELETE("/:userId", middlewares.Admin, deleteUser)
+}
