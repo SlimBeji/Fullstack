8439fac70edb2ad4cb49e88e7465e15c552de39e
Slim Beji <mslimbeji@gmail.com>
2025-10-11 12:18:18 +0100

    register dummy swagger example

--- backend-gin/app.go
+++ backend-gin/app.go
@@ -5,11 +5,16 @@ import (
 	"backend/internal/config"
 	"fmt"
 
+	_ "backend/internal/api/docs"
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func main() {
 	r := gin.Default()
 	routes.RegisterRoutes(r)
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(fmt.Sprintf(":%d", config.Env.Port))
 }
