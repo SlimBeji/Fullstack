a123e6fea51dabf34ae43b5ca15efc5f24c2f5ec
Slim Beji <mslimbeji@gmail.com>
2025-10-11 19:23:41 +0100

    update example in helpers_test.go

--- backend-gin/internal/tests/utils/helpers_test.go
+++ backend-gin/internal/tests/utils/helpers_test.go
@@ -1,10 +1,35 @@
 package tests
 
-import "testing"
+import (
+	"backend/internal/lib/utils"
+	"reflect"
+	"testing"
+)
 
-func TestX(t *testing.T) {
-	x := 3 + 6
-	if x != 9 {
-		t.Errorf("Expected 9, got %d", x)
+func TestRemoveFromList(t *testing.T) {
+	list := []string{"a", "b", "c"}
+
+	// Test case 1: Item not found
+	newList, found := utils.RemoveFromList(list, "d")
+	if found {
+		t.Errorf("Expected false got true")
+	}
+	if !reflect.DeepEqual(newList, list) {
+		t.Errorf("Expected original list when item not found")
+	}
+
+	// Test case 2: Item found
+	newList, found = utils.RemoveFromList(list, "b")
+	if !found {
+		t.Errorf("Expected true got false")
+	}
+	expected := []string{"a", "c"}
+	if !reflect.DeepEqual(newList, expected) {
+		t.Errorf("RemoveFromList did not remove item 'b'. Got %v, expected %v", newList, expected)
+	}
+
+	// Test case 3: Original slice unchanged (important!)
+	if !reflect.DeepEqual(list, []string{"a", "b", "c"}) {
+		t.Errorf("Original slice was modified")
 	}
 }
