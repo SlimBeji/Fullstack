aa2b1d8b23e6f0343670c1b292c6de83f40a6aa2
Slim Beji <mslimbeji@gmail.com>
2025-10-12 19:04:56 +0100

    adding swagger documenattion to Auth endpoints

--- backend-gin/internal/api/routes/auth.go
+++ backend-gin/internal/api/routes/auth.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"backend/internal/api/middlewares"
+	"backend/internal/lib/utils"
 	"backend/internal/models/schemas"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,16 +20,35 @@ func dummyToken() schemas.EncodedToken {
 	}
 }
 
+// @Summary      User registration
+// @Tags         Auth
+// @Accept       mpfd
+// @Produce      json
+// @Param        params formData schemas.SignupForm true "Signup parameters"
+// @Param        image  formData file   true "User's profile image (JPEG)"
+// @Success      200  {object}  schemas.EncodedToken
+// @Router       /api/auth/signup [post]
 func signup(c *gin.Context) {
+	body, _ := utils.GetBody[schemas.SignupForm](c)
+	fmt.Println(body)
 	c.JSON(http.StatusOK, dummyToken())
 }
 
+// @Summary      User authentication
+// @Tags         Auth
+// @Accept       x-www-form-urlencoded
+// @Produce      json
+// @Param        params formData schemas.SigninForm true "Signin parameters"
+// @Success      200  {object}  schemas.EncodedToken
+// @Router       /api/auth/signin [post]
 func signin(c *gin.Context) {
+	body, _ := utils.GetBody[schemas.SigninForm](c)
+	fmt.Println(body)
 	c.JSON(http.StatusOK, dummyToken())
 }
 
 func RegisterAuth(r *gin.Engine) {
 	router := r.Group("/api/auth")
-	router.POST("/signup", middlewares.BodyExtractor, signup)
-	router.POST("/signin", middlewares.BodyExtractor, signin)
+	router.POST("/signup", middlewares.BodyExtractor[schemas.SignupForm], signup)
+	router.POST("/signin", middlewares.BodyExtractor[schemas.SigninForm], signin)
 }
