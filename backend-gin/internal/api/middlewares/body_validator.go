b27bfba67dabe4cca5d2a9aa6617313faedb226b
Slim Beji <mslimbeji@gmail.com>
2025-10-12 16:55:14 +0100

    update Body Validator middleware

--- backend-gin/internal/api/middlewares/body_validator.go
+++ backend-gin/internal/api/middlewares/body_validator.go
@@ -1,20 +1,58 @@
 package middlewares
 
 import (
-	"fmt"
-	"io"
+	"backend/internal/lib/utils"
+	"backend/internal/types_"
 	"net/http"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 )
 
-func BodyExtractor(c *gin.Context) {
-	body, err := io.ReadAll(c.Request.Body)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body"})
-		return
+func BodyExtractor(form any) gin.HandlerFunc {
+	// Get the type of the form to create inside the request handler
+	formType := reflect.TypeOf(form)
+	if formType.Kind() == reflect.Pointer {
+		formType = formType.Elem()
+	} else {
+		panic("BodyExtractor needs a struct pointer")
 	}
-	fmt.Println(string(body))
-	c.Set("requestBody", body)
-	c.Next()
+
+	return func(c *gin.Context) {
+		var err error
+		contentType := c.GetHeader("Content-Type")
+		newForm := reflect.New(formType).Interface()
+
+		switch types_.ContentType(contentType) {
+		case types_.CONTENT_TYPE_MULTIPART, types_.CONTENT_TYPE_FORM_URLENCODED:
+			err = c.ShouldBind(newForm)
+		default:
+			err = c.ShouldBindJSON(newForm)
+		}
+
+		// return bad request if could not do the bind
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error":   "Bad request",
+				"details": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+
+		// Validate the data
+		errs := utils.ValidateStruct(newForm)
+		if len(errs) > 0 {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{
+				"error":   "Invalid request body",
+				"details": errs,
+			})
+			c.Abort()
+			return
+		}
+
+		// Store the parsed form in context for later use
+		c.Set("requestBody", newForm)
+		c.Next()
+	}
 }
