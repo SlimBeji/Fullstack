3ea326d7a213b52786e9a97aaa916bbcc478557c
Slim Beji <mslimbeji@gmail.com>
2025-10-15 16:00:21 +0100

    creating a filters middleware for parsing queries and searches

---
+++ backend-gin/internal/api/middlewares/filters.go
@@ -0,0 +1,75 @@
+package middlewares
+
+import (
+	"backend/internal/models/schemas"
+	"backend/internal/types_"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-viper/mapstructure/v2"
+)
+
+func queryParamsToBody[T any](c *gin.Context) (T, []string) {
+	var body T
+
+	queryParams := c.Request.URL.Query()
+	paramsMap := make(map[string]any)
+	for key, values := range queryParams {
+		if key == "page" || key == "size" {
+			paramsMap[key] = values[0]
+		} else {
+			paramsMap[key] = c.QueryArray(key)
+		}
+	}
+
+	decoderConfig := &mapstructure.DecoderConfig{
+		Result:           &body,
+		TagName:          "json",
+		WeaklyTypedInput: true,
+	}
+
+	decoder, err := mapstructure.NewDecoder(decoderConfig)
+	if err != nil {
+		return body, []string{err.Error()}
+	}
+	if err := decoder.Decode(paramsMap); err != nil {
+		return body, []string{err.Error()}
+	}
+
+	return body, nil
+}
+
+func Filter[T any](c *gin.Context) {
+	var body T
+	var errs []string
+
+	if c.Request.Method == http.MethodGet {
+		body, errs = queryParamsToBody[T](c)
+	} else {
+		body, errs = extractBody[T](c)
+	}
+
+	if len(errs) > 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error":   "Invalid request body",
+			"details": errs,
+		})
+		c.Abort()
+		return
+	}
+
+	form := types_.FindQuery{}
+	errsMap := schemas.BuildFindQuery(body, &form)
+	if len(errsMap) > 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{
+			"error":   "Invalid request",
+			"details": errsMap,
+		})
+		c.Abort()
+		return
+	}
+
+	// Store the parsed form in context for later use
+	c.Set("requestBody", form)
+	c.Next()
+}
