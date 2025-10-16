0fa0d47cf285fb1cf47bf79ddfb86da1f78024cc
Slim Beji <mslimbeji@gmail.com>
2025-10-16 09:51:31 +0100

    create a helper for sending post requests

---
+++ backend-gin/internal/lib/utils/http.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"backend/internal/config"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func PostRequest(url string, form map[string]any, auth ...string) (*http.Response, error) {
+	jsonData, err := json.Marshal(form)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal request body: %w", err)
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if len(auth) > 0 && auth[0] != "" {
+		req.Header.Set("Authorization", "Bearer "+auth[0])
+	}
+
+	client := &http.Client{Timeout: time.Duration(config.Env.DefaultTimeout) * time.Second}
+	return client.Do(req)
+}
