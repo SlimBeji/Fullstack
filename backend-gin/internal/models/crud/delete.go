18b460350994e7e7bcc98e70fd96248de4dca3c4
Slim Beji <mslimbeji@gmail.com>
2025-10-25 18:32:18 +0100

    add delete methods

---
+++ backend-gin/internal/models/crud/delete.go
@@ -0,0 +1,87 @@
+package crud
+
+import (
+	"backend/internal/models/schemas"
+	"backend/internal/types_"
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type DocumentDeleter[Read any] interface {
+	DocumentReader[Read]
+	FindOneAndDelete(context.Context, any, ...*options.FindOneAndDeleteOptions) *mongo.SingleResult
+	PostDelete(mongo.SessionContext) error
+	AuthDelete(*schemas.UserRead, *Read) error
+}
+
+func DeleteDocument[Read any](
+	dd DocumentDeleter[Read], filter bson.M, ctx context.Context,
+) error {
+	client := dd.Database().Client()
+	session, err := client.StartSession()
+	if err != nil {
+		return err
+	}
+	defer session.EndSession(ctx)
+
+	return mongo.WithSession(
+		ctx,
+		session,
+		func(sc mongo.SessionContext) error {
+			if err := session.StartTransaction(); err != nil {
+				return err
+			}
+
+			deleteResult := dd.FindOneAndDelete(ctx, filter)
+			if err := deleteResult.Err(); err != nil {
+				session.AbortTransaction(sc)
+				return err
+			}
+
+			if err := dd.PostDelete(sc); err != nil {
+				session.AbortTransaction(sc)
+				return err
+			}
+
+			if err := session.CommitTransaction(sc); err != nil {
+				return err
+			}
+			return nil
+		},
+	)
+}
+
+func Delete[Read any](
+	dd DocumentDeleter[Read], id string, ctx context.Context,
+) error {
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return types_.UnprocessableErr(
+			fmt.Sprintf("invalid object ID %s", id), err,
+		)
+	}
+	return DeleteDocument(dd, bson.M{"_id": objectId}, ctx)
+}
+
+func UserDelete[Read any](
+	dd DocumentDeleter[Read],
+	user *schemas.UserRead,
+	id string,
+	ctx context.Context,
+) error {
+	doc, err := UserGetById(dd, user, id, ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := dd.AuthDelete(user, &doc); err != nil {
+		return err
+	}
+
+	return Delete(dd, id, ctx)
+}
