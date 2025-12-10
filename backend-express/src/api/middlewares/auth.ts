a6a10d9802e97deba5c730ee50f99f7993f36e32
Slim BEJI <mslimbeji@gmail.com>
2025-12-10 10:29:25 +0100

    deprecate auth module

--- backend-express/src/api/middlewares/auth.ts
+++ backend-express/src/api/middlewares/auth.ts
@@ -2,8 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import { TokenExpiredError } from "jsonwebtoken";
 
 import { ApiError, HttpStatus } from "@/lib/express";
+import { crudUser } from "@/models/crud";
+import { decodeToken, UserRead } from "@/models/schemas";
 
-import { getUserFromToken } from "../auth";
+const getUserFromToken = async (token: string): Promise<UserRead> => {
+    const payload = decodeToken(token);
+    const user = await crudUser.get(payload.userId);
+    if (!user) {
+        throw new ApiError(HttpStatus.NOT_FOUND, "User not found");
+    }
+    if (user.email !== payload.email) {
+        throw new ApiError(
+            HttpStatus.BAD_REQUEST,
+            "Invalid token, payload corrupted"
+        );
+    }
+    return user;
+};
 
 const getError = (message: string): ApiError => {
     return new ApiError(HttpStatus.UNAUTHORIZED, message);
