75bff0760472f017f3797eaf6797243c2a2dfb50
Slim BEJI <mslimbeji@gmail.com>
2025-12-17 20:15:53 +0100

    deprecate auth package - move everything to auth.go in middlewares

--- backend-gin/internal/api/middlewares/auth.go
+++ backend-gin/internal/api/middlewares/auth.go
@@ -1,13 +1,67 @@
 package middlewares
 
 import (
-	"backend/internal/api/auth"
+	"backend/internal/config"
 	"backend/internal/lib/gin_"
+	"backend/internal/lib/utils"
+	"backend/internal/models/collections"
+	"backend/internal/models/schemas"
+	"context"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+func GetUserFromToken(token string) (schemas.UserRead, error) {
+	badToken := gin_.ApiError{
+		Code:    http.StatusUnauthorized,
+		Message: "Token Not Valid",
+	}
+
+	payload, err := utils.DecodePayload(token, config.Env.SecretKey)
+	if err != nil {
+		if strings.Contains(err.Error(), "token expired") {
+			return schemas.UserRead{}, gin_.ApiError{
+				Code:    http.StatusUnauthorized,
+				Message: "Token Expired",
+			}
+		}
+		return schemas.UserRead{}, badToken
+	}
+
+	userIdRaw, userFound := payload["userId"]
+	emailRaw, emailFound := payload["email"]
+	if !userFound || !emailFound {
+		return schemas.UserRead{}, badToken
+	}
+
+	userId, userIdValid := userIdRaw.(string)
+	email, emailValid := emailRaw.(string)
+	if !userIdValid || !emailValid {
+		return schemas.UserRead{}, badToken
+	}
+
+	uc := collections.GetUserCollection()
+	ctx := context.Background()
+	user, err := uc.GetById(userId, ctx)
+	if err != nil {
+		return schemas.UserRead{}, gin_.ApiError{
+			Code:    http.StatusNotFound,
+			Message: "user not found",
+		}
+	}
+
+	if user.Email != email {
+		return schemas.UserRead{}, gin_.ApiError{
+			Code:    http.StatusBadRequest,
+			Message: "Invalid token, payload corrupted",
+		}
+	}
+
+	return user, nil
+}
 
 func checkAuthToken(checkAdmin bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -32,7 +86,7 @@ func checkAuthToken(checkAdmin bool) gin.HandlerFunc {
 			return
 		}
 		authtoken := match[1]
-		user, err := auth.GetUserFromToken(authtoken)
+		user, err := GetUserFromToken(authtoken)
 		if err != nil {
 			gin_.AbortWithStatusJSON(c, err)
 			return
