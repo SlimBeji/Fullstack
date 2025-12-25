760f521b0105b34b361564d968c94eab9e1bc935
Slim BEJI <mslimbeji@gmail.com>
2025-12-25 18:17:42 +0100

    implement debug trait for FileToUpload

--- backend-axum/src/lib_/types_/upload.rs
+++ backend-axum/src/lib_/types_/upload.rs
@@ -1,6 +1,17 @@
-#[derive(Debug, Clone)]
+use std::fmt;
+
+#[derive(Clone)]
 pub struct FileToUpload {
     pub originalname: String,
     pub mimetype: String,
     pub data: Vec<u8>,
+}
+
+impl fmt::Debug for FileToUpload {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("FileToUpload")
+            .field("originalname", &self.originalname)
+            .field("mimetype", &self.mimetype)
+            .finish()
+    }
 }
