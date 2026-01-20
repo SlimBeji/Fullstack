5ea821b28d34cfc80d377abb2f2145c352f25a28
Slim BEJI <mslimbeji@gmail.com>
2026-01-20 09:46:13 +0100

    register new validator_ mocdles

--- backend-axum/src/lib_/validator_/mod.rs
+++ backend-axum/src/lib_/validator_/mod.rs
@@ -1,7 +1,12 @@
+pub mod filters;
 pub mod regex;
 pub mod rules;
+pub mod utils;
 
+pub use filters::FieldFilters;
+pub use filters::FiltersReader;
 pub use rules::array_length;
 pub use rules::email_strict;
 pub use rules::object_id;
 pub use rules::string_length;
+pub use utils::errors_to_serde_map;
