f8f118cc9272badd5d547a195cdf55e070fd7766
Slim BEJI <mslimbeji@gmail.com>
2026-02-01 21:42:37 +0100

    move prisma generated client to _generated folder

---
+++ backend-express/src/bin/testPg.ts
@@ -0,0 +1,12 @@
+import { pgClient } from "@/services/instances";
+import { closeDbs, connectDbs } from "@/services/setup";
+
+async function debug() {
+    const tables = await pgClient.client
+        .$queryRaw`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`;
+    console.log(tables);
+}
+
+if (require.main === module) {
+    connectDbs().then(debug).finally(closeDbs);
+}
