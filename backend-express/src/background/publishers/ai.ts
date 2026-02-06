d8ccf656d2c57eaaef591aa940f0a82b4f695cce
Slim BEJI <mslimbeji@gmail.com>
2026-02-06 16:18:25 +0100

    update placeEmbedding signature

--- backend-express/src/background/publishers/ai.ts
+++ backend-express/src/background/publishers/ai.ts
@@ -1,7 +1,7 @@
 import { Queues, TASK_PLACE_EMBEDDING } from "../setup";
 import { publisher } from "./publisher";
 
-export const placeEmbedding = (placeId: string) => {
+export const placeEmbedding = (placeId: number) => {
     if (process.env.JEST_WORKER_ID) return;
     let queue = publisher.getQueue(Queues.AI);
     queue.add(TASK_PLACE_EMBEDDING, { placeId });
