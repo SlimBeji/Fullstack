1a13aefb046767ad31074e1a544cea923c83b7a7
Slim BEJI <mslimbeji@gmail.com>
2026-02-08 09:15:29 +0100

    use same migration files for test and dev database

--- backend-express/src/models/migrations/prisma.dev.config.ts
+++ backend-express/src/models/migrations/prisma.dev.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
     schema: "schema.prisma",
     migrations: {
-        path: "prisma/migrations/dev",
+        path: "prisma/migrations",
     },
     datasource: {
         url: env("DATABASE_URL"),
--- backend-express/src/models/migrations/prisma.test.config.ts
+++ backend-express/src/models/migrations/prisma.test.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
     schema: "schema.prisma",
     migrations: {
-        path: "prisma/migrations/test",
+        path: "prisma/migrations",
     },
     datasource: {
         url: env("DATABASE_TEST_URL"),
