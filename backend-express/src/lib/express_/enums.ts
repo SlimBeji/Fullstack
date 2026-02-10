9fd7501548ca9dd7fccaddfbcc438653dc3cd281
Slim BEJI <mslimbeji@gmail.com>
2026-02-10 19:26:03 +0100

    add Conflict http status

--- backend-express/src/lib/express_/enums.ts
+++ backend-express/src/lib/express_/enums.ts
@@ -3,6 +3,7 @@ export enum HttpStatus {
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
     NOT_FOUND = 404,
+    CONFLICT = 409,
     UNPROCESSABLE_ENTITY = 422,
     INTERNAL_SERVER_ERROR = 500,
 }
