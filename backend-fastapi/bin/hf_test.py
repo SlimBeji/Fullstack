97f5ced4cd8de0966de31c67da949c8914a54263
Slim BEJI <mslimbeji@gmail.com>
2026-02-19 09:40:53 +0100

    update hf_test script

--- backend-fastapi/bin/hf_test.py
+++ backend-fastapi/bin/hf_test.py
@@ -1,25 +1,11 @@
 import asyncio
-from typing import Any
 
 from services.instances import hf_client
-from services.setup import close_all, start_all
-
-
-def echo(output: Any):
-    print("--------------------------")
-    print(output)
-    print("--------------------------")
-
-
-async def debug():
-    vec = await hf_client.embed_text("I am trying to debug my code in go")
-    echo(vec)
 
 
 async def main():
-    await start_all()
-    await debug()
-    await close_all()
+    vec = await hf_client.embed_text("I am trying to debug my code in python")
+    print(vec)
 
 
 if __name__ == "__main__":
