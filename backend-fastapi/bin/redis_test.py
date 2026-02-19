f0035840898496cf5f4d787c23c3d56af0f0a0c0
Slim BEJI <mslimbeji@gmail.com>
2026-02-19 09:37:46 +0100

    redis tests

--- backend-fastapi/bin/redis_test.py
+++ backend-fastapi/bin/redis_test.py
@@ -1,27 +1,19 @@
 import asyncio
-from typing import Any
 
 from services.instances import redis_client
-from services.setup import close_all, start_all
-
-
-def echo(output: Any):
-    print("--------------------------")
-    print(output)
-    print("--------------------------")
 
 
 async def debug():
     await redis_client.set("secret_number", 158)
-    echo(await redis_client.get("secret_number", "int"))
+    print(await redis_client.get("secret_number", "int"))
     await redis_client.delete("secret_number")
-    echo(await redis_client.get("secret_number", "int"))
+    print(await redis_client.get("secret_number", "int"))
 
 
 async def main():
-    await start_all()
+    await redis_client.connect()
     await debug()
-    await close_all()
+    await redis_client.close()
 
 
 if __name__ == "__main__":
