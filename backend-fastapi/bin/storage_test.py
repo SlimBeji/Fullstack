1ab9a09bf450a2242d85fdb17a33e36ee8f291d6
Slim BEJI <mslimbeji@gmail.com>
2026-02-19 09:33:48 +0100

    update storage test script

--- backend-fastapi/bin/storage_test.py
+++ backend-fastapi/bin/storage_test.py
@@ -1,28 +1,14 @@
 import asyncio
-from typing import Any
 
 from services.instances import cloud_storage
-from services.setup import close_all, start_all
 from static import get_image_path
 
 
-def echo(output: Any):
-    print("--------------------------")
-    print(output)
-    print("--------------------------")
-
-
-async def debug():
+async def main():
     path = get_image_path("avatar1.jpg")
     destination = cloud_storage.upload_file(path)
     url = cloud_storage.get_signed_url(destination)
     print(url)
-
-
-async def main():
-    await start_all()
-    await debug()
-    await close_all()
 
 
 if __name__ == "__main__":
