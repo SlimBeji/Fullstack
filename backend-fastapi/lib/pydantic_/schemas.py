d5c97602f3bc79d9ad23c96b74c80136f75bf6af
Slim BEJI <mslimbeji@gmail.com>
2026-02-19 10:47:03 +0100

    update BaseSearchSchema

--- backend-fastapi/lib/pydantic_/schemas.py
+++ backend-fastapi/lib/pydantic_/schemas.py
@@ -1,15 +1,16 @@
 from typing import Annotated, Generic, TypeVar, cast
 
 from pydantic import BaseModel, Field
-from pydantic.fields import ModelPrivateAttr
+from pydantic.fields import FieldInfo, ModelPrivateAttr
 
 SortableFields = TypeVar("SortableFields")
-
 SelectableFields = TypeVar("SelectableFields")
 
 
-class BaseFiltersSchema(BaseModel, Generic[SelectableFields, SortableFields]):
+class BaseSearchSchema(BaseModel, Generic[SelectableFields, SortableFields]):
     _MAX_SIZE: int = 100
+    _DEFAULT_SORT: list[str] = ["-createdAt"]
+    _DEFAULT_FIELDS: list[str] = ["id"]
 
     page: Annotated[int, Field(1, description="The page number")]
     size: Annotated[int, Field(_MAX_SIZE, description="Items per page")]
@@ -17,19 +18,41 @@ class BaseFiltersSchema(BaseModel, Generic[SelectableFields, SortableFields]):
         list[SortableFields] | None,
         Field(
             description="Fields to use for sorting. Use '-' for descending",
-            examples=[["-createdAt"]],
+            examples=[_DEFAULT_SORT],
         ),
     ] = None
     fields: Annotated[
         list[SelectableFields] | None,
         Field(
             description="Fields to include in the response; omit for full document",
-            examples=[["id"]],
+            examples=[_DEFAULT_FIELDS],
         ),
     ] = None
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
-        private_field = cast(ModelPrivateAttr, cls._MAX_SIZE)
-        max_size = private_field.default
+
+        # Update size default
+        private_max_size = cast(ModelPrivateAttr, cls._MAX_SIZE)
+        max_size = private_max_size.default
         cls.model_fields["size"].default = max_size
+
+        # Update sort default and examples
+        default_sort = cast(ModelPrivateAttr, cls._DEFAULT_SORT)
+        sort_field = cls.model_fields["sort"]
+        cls.model_fields["sort"] = FieldInfo(
+            annotation=sort_field.annotation,
+            default=default_sort.default,
+            description=sort_field.description,
+            examples=[default_sort.default],
+        )
+
+        # Update fields default and examples
+        default_fields = cast(ModelPrivateAttr, cls._DEFAULT_FIELDS)
+        fields_field = cls.model_fields["fields"]
+        cls.model_fields["fields"] = FieldInfo(
+            annotation=fields_field.annotation,
+            default=default_fields.default,
+            description=fields_field.description,
+            examples=[default_fields.default],
+        )
