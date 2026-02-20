77f2960545c037ab56803b4f16d0761c71de016b
Slim BEJI <mslimbeji@gmail.com>
2026-02-20 18:44:07 +0100

    minor openapi update

--- backend-fastapi/api/docs/openapi.py
+++ backend-fastapi/api/docs/openapi.py
@@ -1,6 +1,6 @@
 OPENAPI_METADATA = [
     dict(name="Auth", description="Registration and Authentication endpoints"),
     dict(name="Hello World", description="Hello World endpoints"),
-    dict(name="User", description="User crud endpoints"),
-    dict(name="Place", description="Place crud endpoints"),
+    dict(name="User", description="User cruds endpoints"),
+    dict(name="Place", description="Place cruds endpoints"),
 ]
