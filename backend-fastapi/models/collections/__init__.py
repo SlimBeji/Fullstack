d36bed7c3f570eabd9fbe392239866d0040963c4
Slim BEJI <mslimbeji@gmail.com>
2026-02-20 11:16:41 +0100

    deprecate the collections module

--- backend-fastapi/models/collections/__init__.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-
-from models.collections.base import *
-from models.collections.place import *
-from models.collections.user import *
-
-User.model_rebuild()  # noqa: F405
-Place.model_rebuild()  # noqa: F405
-document_models: list[type[Document]] = [User, Place]  # noqa: F405
