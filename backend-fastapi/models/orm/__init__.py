16702804c5d6ccf72682bd62d5d6b68c895d9a9a
Slim BEJI <mslimbeji@gmail.com>
2026-02-20 18:20:29 +0100

    create enums for models/tables

--- backend-fastapi/models/orm/__init__.py
+++ backend-fastapi/models/orm/__init__.py
@@ -1,3 +1,4 @@
 from lib.sqlalchemy_ import BaseModel
 from models.orm.place import *
+from models.orm.tables import *
 from models.orm.user import *
---
+++ backend-fastapi/models/orm/tables.py
@@ -0,0 +1,11 @@
+from lib.utils import StrEnum
+
+
+class Tables(StrEnum):
+    USERS = "users"
+    PLACES = "places"
+
+
+class Models(StrEnum):
+    USER = "User"
+    PLACE = "Place"
