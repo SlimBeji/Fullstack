77f258052f6605dcc811f29019a14ffbc7e51316
Slim BEJI <mslimbeji@gmail.com>
2026-02-20 18:20:45 +0100

    update the seed script

--- backend-fastapi/models/examples/seed.py
+++ backend-fastapi/models/examples/seed.py
@@ -1,31 +1,22 @@
 import asyncio
 
-from beanie.odm.fields import PydanticObjectId
-
-from models.collections import CollectionEnum, document_models
-from models.crud import crud_place, crud_user
+from models.cruds import CrudsPlace, CrudsUser
 from models.examples.places import PLACES
 from models.examples.users import USERS
+from models.orm import Tables
 from models.schemas import (
     PlaceCreateSchema,
     PlaceSeedSchema,
     UserCreateSchema,
     UserSeedSchema,
 )
-from services.instances import cloud_storage, db, redis_client
+from services.instances import cloud_storage, pg_client, redis_client
 
-USER_REF_MAPPING: dict[int, str] = {}
-PLACE_REF_MAPPING: dict[int, str] = {}
-
-
-async def create_collections() -> None:
-    coroutines = []
-    for name in CollectionEnum.values():
-        coroutines.append(db.create_collection(name))
-    await asyncio.gather(*coroutines)
+USER_REF_MAPPING: dict[int, int] = {}
+PLACE_REF_MAPPING: dict[int, int] = {}
 
 
-async def _seed_user(user: UserSeedSchema) -> None:
+async def _seed_user(cruds: CrudsUser, user: UserSeedSchema) -> None:
     if user.imageUrl:
         imageUrl = cloud_storage.upload_file(user.imageUrl)
     else:
@@ -33,51 +24,54 @@ async def _seed_user(user: UserSeedSchema) -> None:
 
     data = user.model_dump()
     data["imageUrl"] = imageUrl
-    document = await crud_user.create_document(UserCreateSchema(**data))
-    USER_REF_MAPPING[user.ref] = str(document.id)
+    id = await cruds.create(UserCreateSchema(**data))
+    USER_REF_MAPPING[user.ref] = id
 
 
-async def seed_users(users: list[UserSeedSchema]) -> None:
-    couroutines = [_seed_user(u) for u in users]
+async def seed_users(cruds: CrudsUser, users: list[UserSeedSchema]) -> None:
+    couroutines = [_seed_user(cruds, u) for u in users]
     await asyncio.gather(*couroutines)
 
 
-async def _seed_place(place: PlaceSeedSchema) -> None:
+async def _seed_place(cruds: CrudsPlace, place: PlaceSeedSchema) -> None:
     if place.imageUrl:
         imageUrl = cloud_storage.upload_file(place.imageUrl)
     else:
         imageUrl = ""
 
-    creatorId = PydanticObjectId(USER_REF_MAPPING[place.creator_ref])
+    creatorId = USER_REF_MAPPING[place.creator_ref]
     data = place.model_dump()
     data["imageUrl"] = imageUrl
     data["creatorId"] = creatorId
-    document = await crud_place.create_document(PlaceCreateSchema(**data))
-    PLACE_REF_MAPPING[place.ref] = str(document.id)
+    embedding = data.pop("embedding")
+    record = await cruds.seed(PlaceCreateSchema(**data), embedding)
+    PLACE_REF_MAPPING[place.ref] = record.id
 
 
-async def seed_places(places: list[PlaceSeedSchema]) -> None:
-    couroutines = [_seed_place(p) for p in places]
+async def seed_places(crud: CrudsPlace, places: list[PlaceSeedSchema]) -> None:
+    couroutines = [_seed_place(crud, p) for p in places]
     await asyncio.gather(*couroutines)
 
 
 async def seed_db(verbose: bool = False) -> None:
-    await create_collections()
-    await seed_users(USERS)
-    if verbose:
-        print("✅ Collection User seeded!")
-    await seed_places(PLACES)
-    if verbose:
-        print("✅ Collection Place seeded!")
-    if verbose:
-        print("✅ Finished. You may exit")
+    async with pg_client.session() as session:
+        cruds_user = CrudsUser(session)
+        cruds_place = CrudsPlace(session)
+        await seed_users(cruds_user, USERS)
+        if verbose:
+            print("✅ Collection User seeded!")
+        await seed_places(cruds_place, PLACES)
+        if verbose:
+            print("✅ Collection Place seeded!")
+        if verbose:
+            print("✅ Finished. You may exit")
 
 
 async def dump_db(verbose: bool = False) -> None:
-    for document in document_models:
-        await document.delete_all()
+    for tablename in Tables.values():
+        await pg_client.reset_table(tablename)
         if verbose:
-            print(f"✅ Collection {document.__name__} cleared!")
+            print(f"✅ Collection {tablename} cleared!")
 
     await redis_client.flushall()
     if verbose:
