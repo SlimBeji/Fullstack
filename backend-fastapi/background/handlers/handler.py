7899f2670e4419c4d7ccb2eccf5064a6f53c8c6a
Slim BEJI <mslimbeji@gmail.com>
2026-02-26 12:43:34 +0100

    update worker starting point

--- backend-fastapi/background/handlers/handler.py
+++ backend-fastapi/background/handlers/handler.py
@@ -24,4 +24,6 @@ async def connect_dbs() -> None:
 
 
 handler = TaskHandler(BROKER_URL, TASKS, connect_dbs, MAX_AGE, settings.is_test)
-handler.start()
+
+if __name__ == "__main__":
+    handler.start()
