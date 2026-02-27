3c7057583bba6a22c0205ff28b904a76f5d67d8b
Slim BEJI <mslimbeji@gmail.com>
2026-02-27 07:56:39 +0100

    refactor redis test script

--- backend-gin/internal/bin/test_redis.go
+++ backend-gin/internal/bin/test_redis.go
@@ -2,22 +2,22 @@ package bin
 
 import (
 	"backend/internal/services/instances"
-	"backend/internal/services/setup"
 	"context"
 	"fmt"
 )
 
 func TestRedis() {
-	// setup
-	setup := setup.New()
-	defer setup.CloseSerivces()
-
+	// Get redis client
 	ctx := context.Background()
 	redis := instances.GetRedisClient()
 	defer redis.Close()
+
+	// Test setting and retrieving value
 	redis.Set(ctx, "secret_number", 158)
 	result, _ := redis.Get(ctx, "secret_number")
 	fmt.Println(result)
+
+	// Test setting and deleting value
 	redis.Delete(ctx, "secret_number")
 	result, _ = redis.Get(ctx, "secret_number")
 	fmt.Println(result)
