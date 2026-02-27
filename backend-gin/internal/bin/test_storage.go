c3ce86b8ceacf8cae1f5d61b75dca8a8c2610ec3
Slim BEJI <mslimbeji@gmail.com>
2026-02-27 07:56:47 +0100

    refactor storage test script

--- backend-gin/internal/bin/test_storage.go
+++ backend-gin/internal/bin/test_storage.go
@@ -2,25 +2,25 @@ package bin
 
 import (
 	"backend/internal/services/instances"
-	"backend/internal/services/setup"
 	"backend/internal/static"
 	"context"
 	"fmt"
 )
 
 func TestStorage() {
-	setup := setup.New()
-	defer setup.CloseSerivces()
-
+	// Get Storage client
 	ctx := context.Background()
 	path := static.GetImagePath("avatar1.jpg")
 	storage := instances.GetStorage()
 	defer storage.Close()
+
+	// Test uploading a file
 	destination, err := storage.UploadFile(ctx, path, "")
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
+	// Test a getting a signed URL
 	url, err := storage.GetSignedURL(destination, 3600)
 	if err != nil {
 		fmt.Println(err)
