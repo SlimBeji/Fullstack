7539d9044375d8c281a37976b3f6b3b0de6b1298
Slim BEJI <mslimbeji@gmail.com>
2026-03-23 17:48:11 +0100

    update pagination examples

--- backend-axum/src/lib/types_/pagination.rs
+++ backend-axum/src/lib/types_/pagination.rs
@@ -5,11 +5,14 @@ use utoipa::ToSchema;
 #[derive(Debug, Serialize, ToSchema)]
 #[serde(rename_all = "camelCase")]
 pub struct PaginatedData<T: Debug + Serialize + ToSchema> {
-    #[schema(example = "2")]
+    #[schema(example = "1")]
     pub page: usize,
-    #[schema(example = "10")]
+
+    #[schema(example = "2")]
     pub total_pages: usize,
-    #[schema(example = "197")]
+
+    #[schema(example = "40")]
     pub total_count: usize,
+
     pub data: Vec<T>,
 }
