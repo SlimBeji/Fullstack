05743a738cc8ddd5f83e790d285fb5db017c781f
Slim BEJI <mslimbeji@gmail.com>
2026-03-26 18:50:41 +0100

    rename cache.ts to redis_.ts

--- backend-express/src/lib/clients/index.ts
+++ backend-express/src/lib/clients/index.ts
@@ -1,6 +1,6 @@
-export * from "./cache";
 export * from "./huggingFace";
 export * from "./pgsql";
+export * from "./redis_";
 export * from "./scheduler";
 export * from "./storage";
 export * from "./worker";
