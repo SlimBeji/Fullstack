fb59c4e0d82944b413a5b08b68584b677dbce65e
Slim BEJI <mslimbeji@gmail.com>
2026-03-28 11:53:36 +0100

    create seaorm models

--- backend-axum/src/models/orm/place.rs
+++ backend-axum/src/models/orm/place.rs
@@ -1,25 +1,60 @@
+use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
 
-#[derive(Debug, Serialize, Deserialize)]
+use crate::lib_::seaorm_::derive_timestamp_update;
+
+#[derive(
+    Clone, Debug, PartialEq, Serialize, Deserialize, FromJsonQueryResult,
+)]
 pub struct Location {
     pub lat: f64,
     pub lng: f64,
 }
 
-#[allow(dead_code)] // to be removed
-#[derive(Debug, Serialize, Deserialize)]
-pub struct PlaceDB {
-    pub id: u32,
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "places")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+
+    #[sea_orm(column_type = "TimestampWithTimeZone", indexed)]
+    pub created_at: DateTimeWithTimeZone,
+
+    #[sea_orm(column_type = "TimestampWithTimeZone")]
+    pub updated_at: DateTimeWithTimeZone,
+
     pub title: String,
     pub description: String,
     pub address: String,
+
+    #[sea_orm(default_value = "")]
+    pub image_url: String,
+
+    #[sea_orm(column_type = "Json")]
     pub location: Location,
-    pub image_url: Option<String>,
-    pub embedding: Option<Vec<f64>>,
-    pub creator_id: u32,
-    #[serde(with = "time::serde::rfc3339")]
-    pub created_at: OffsetDateTime,
-    #[serde(with = "time::serde::rfc3339")]
-    pub updated_at: OffsetDateTime,
+
+    #[sea_orm(column_type = "custom(\"vector(384)\")", nullable)]
+    pub embedding: Option<Vec<f32>>,
+
+    #[sea_orm(indexed)]
+    pub creator_id: i32,
 }
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::CreatorId",
+        to = "super::user::Column::Id",
+        on_delete = "Cascade"
+    )]
+    Creator,
+}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Creator.def()
+    }
+}
+
+derive_timestamp_update!(ActiveModel);
--- backend-axum/src/models/orm/user.rs
+++ backend-axum/src/models/orm/user.rs
@@ -1,18 +1,43 @@
-use serde::{Deserialize, Serialize};
-use time::OffsetDateTime;
+use sea_orm::entity::prelude::*;
 
-#[allow(dead_code)] // to be removed
-#[derive(Debug, Serialize, Deserialize)]
-pub struct UserDB {
-    pub id: u32,
+use crate::lib_::seaorm_::derive_timestamp_update;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "users")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+
+    #[sea_orm(column_type = "TimestampWithTimeZone", indexed)]
+    pub created_at: DateTimeWithTimeZone,
+
+    #[sea_orm(column_type = "TimestampWithTimeZone")]
+    pub updated_at: DateTimeWithTimeZone,
+
     pub name: String,
+
+    #[sea_orm(unique)]
     pub email: String,
-    pub is_admin: bool,
+
     pub password: String,
-    pub image_url: Option<String>,
-    pub places: Vec<String>,
-    #[serde(with = "time::serde::rfc3339")]
-    pub created_at: OffsetDateTime,
-    #[serde(with = "time::serde::rfc3339")]
-    pub updated_at: OffsetDateTime,
+
+    #[sea_orm(default_value = "")]
+    pub image_url: String,
+
+    #[sea_orm(default_value = false)]
+    pub is_admin: bool,
 }
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(has_many = "super::place::Entity")]
+    Places,
+}
+
+impl Related<super::place::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Places.def()
+    }
+}
+
+derive_timestamp_update!(ActiveModel);
