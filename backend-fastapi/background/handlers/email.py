efcd2a5b91faa3523b838e7d2d35a1a8eecdad8f
Slim BEJI <mslimbeji@gmail.com>
2026-04-18 12:15:42 +0100

    fix syntax errors

--- backend-fastapi/background/handlers/email.py
+++ backend-fastapi/background/handlers/email.py
@@ -5,4 +5,4 @@ from background.bgconfig import NewsletterData
 
 def send_newsletter_task(payload: NewsletterData):
     time.sleep(2)
-    print(f"Newsletter sent to {payload["name"]} at {payload["email"]}")
+    print(f"Newsletter sent to {payload['name']} at {payload['email']}")
