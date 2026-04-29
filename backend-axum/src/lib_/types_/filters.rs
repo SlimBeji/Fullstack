a3183a2492bc503e402158e3bc412efe6acf375a
Slim BEJI <mslimbeji@gmail.com>
2026-04-29 12:15:43 +0100

    split filters to filters.rs + search.rs

--- backend-axum/src/lib_/types_/filters.rs
+++ backend-axum/src/lib_/types_/filters.rs
@@ -886,37 +886,3 @@ impl FiltersReader {
 }
 
 pub type WhereFilters = HashMap<String, FieldFilters>;
-
-#[derive(Debug)]
-pub struct SearchQuery {
-    pub page: Option<usize>,
-    pub size: Option<usize>,
-    pub order_by: Option<Vec<String>>,
-    pub select: Option<Vec<String>>,
-    pub where_: Option<WhereFilters>,
-}
-
-impl SearchQuery {
-    pub fn id(id: u32) -> Self {
-        let id_filter = IndexFilters {
-            eq: Some(id),
-            ne: None,
-            null: None,
-            in_: None,
-            nin: None,
-        };
-        let filter = FieldFilters::Index(id_filter);
-        let filters = HashMap::from([("id".to_string(), filter)]);
-        Self {
-            page: None,
-            size: None,
-            order_by: None,
-            select: None,
-            where_: Some(filters),
-        }
-    }
-}
-
-pub trait ToSearchQuery {
-    fn to_search_query(self) -> Result<SearchQuery, ValidationErrors>;
-}
--- backend-axum/src/lib_/types_/mod.rs
+++ backend-axum/src/lib_/types_/mod.rs
@@ -1,14 +1,15 @@
 pub mod errors;
 pub mod filters;
 pub mod pagination;
+pub mod search;
 pub mod upload;
 
 pub use errors::ApiError;
 pub use filters::FieldFilters;
 pub use filters::FilterOp;
 pub use filters::FiltersReader;
-pub use filters::SearchQuery;
-pub use filters::ToSearchQuery;
 pub use filters::WhereFilters;
 pub use pagination::PaginatedData;
+pub use search::SearchQuery;
+pub use search::ToSearchQuery;
 pub use upload::FileToUpload;
