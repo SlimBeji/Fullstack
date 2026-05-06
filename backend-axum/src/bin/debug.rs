1c9a0c76caae8cf34081e70203524cf37628558d
Slim BEJI <mslimbeji@gmail.com>
2026-05-06 11:49:03 +0100

    test script in debug

--- backend-axum/src/bin/debug.rs
+++ backend-axum/src/bin/debug.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Running debug script")
+use backend::{
+    lib_::seaorm_::cruds::Read, models::cruds::CrudsUser, services::instances::AppState,
+};
+
+#[tokio::main]
+async fn main() {
+    let app_state = AppState::new().await;
+    let cruds_user = CrudsUser::new(app_state);
+    let user = cruds_user.get(1).await.expect("could not extract user");
+    println!("{:?}", user)
 }
