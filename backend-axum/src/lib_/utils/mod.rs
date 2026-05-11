5274dd6c3cc93e56754c9b6b0e67cc9efc4c4776
Slim BEJI <mslimbeji@gmail.com>
2026-05-11 17:11:07 +0100

    linting

--- backend-axum/src/lib_/utils/mod.rs
+++ backend-axum/src/lib_/utils/mod.rs
@@ -1,17 +1,11 @@
+pub mod concurency;
 pub mod encryption;
 pub mod helpers;
 pub mod json;
 
-pub use encryption::decode_payload;
-pub use encryption::encode_payload;
-pub use encryption::hash_input;
-pub use encryption::is_hashed;
-pub use encryption::verify_hash;
-pub use helpers::parse_bool;
-pub use helpers::parse_datetime;
-pub use json::get_bool_from_json;
-pub use json::get_datetime_from_json;
-pub use json::get_id_from_json;
-pub use json::get_string_from_json;
-pub use json::parse_enum_array;
-pub use json::unwrap_json_value;
+pub use encryption::{decode_payload, encode_payload, hash_input, is_hashed, verify_hash};
+pub use helpers::{parse_bool, parse_datetime};
+pub use json::{
+    get_bool_from_json, get_datetime_from_json, get_id_from_json, get_string_from_json,
+    parse_enum_array, unwrap_json_value,
+};
