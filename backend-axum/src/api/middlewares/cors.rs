2a32ebbaeee63cbc25bb1e3395feb8b2bf989ec9
Slim BEJI <mslimbeji@gmail.com>
2026-05-21 13:17:40 +0100

    update cors middleware

--- backend-axum/src/api/middlewares/cors.rs
+++ backend-axum/src/api/middlewares/cors.rs
@@ -1,13 +1,10 @@
 use axum::http::{HeaderName, HeaderValue, Method};
-use tower_http::cors::CorsLayer;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::config;
 
 pub fn cors_layer() -> CorsLayer {
-    CorsLayer::new()
-        .allow_origin(
-            HeaderValue::from_str(&config::ENV.app_url).expect("not a valid allow origin value"),
-        )
+    let layer = CorsLayer::new()
         .allow_methods([
             Method::GET,
             Method::POST,
@@ -21,5 +18,13 @@ pub fn cors_layer() -> CorsLayer {
             axum::http::header::ACCEPT,
             axum::http::header::AUTHORIZATION,
             HeaderName::from_static("x-requested-with"),
-        ])
+        ]);
+
+    if config::ENV.app_url == "*" {
+        layer.allow_origin(Any)
+    } else {
+        layer.allow_origin(
+            HeaderValue::from_str(&config::ENV.app_url).expect("not a valid allow origin value"),
+        )
+    }
 }
