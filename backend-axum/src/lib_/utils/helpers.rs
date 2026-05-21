93e798707e6a470f53ce545c28cb4dd1e7e16a57
Slim BEJI <mslimbeji@gmail.com>
2026-05-21 11:53:27 +0100

    update the parse_datetime utils

--- backend-axum/src/lib_/utils/helpers.rs
+++ backend-axum/src/lib_/utils/helpers.rs
@@ -1,6 +1,7 @@
 use time::error::Parse;
+use time::format_description::BorrowedFormatItem;
 use time::format_description::well_known::{Iso8601, Rfc3339};
-use time::{Date, OffsetDateTime, Time};
+use time::{Date, OffsetDateTime, PrimitiveDateTime, Time};
 
 pub fn parse_bool(s: &str) -> Result<bool, String> {
     match s {
@@ -10,9 +11,14 @@ pub fn parse_bool(s: &str) -> Result<bool, String> {
     }
 }
 
+const DATETIME_NO_SECS: &[BorrowedFormatItem<'_>] =
+    time::macros::format_description!("[year]-[month]-[day] [hour]:[minute]");
+
 pub fn parse_datetime(s: &str) -> Result<OffsetDateTime, Parse> {
     let s = s.trim();
-    OffsetDateTime::parse(s, &Rfc3339).or_else(|_| {
-        Date::parse(s, &Iso8601::DATE).map(|d| d.with_time(Time::MIDNIGHT).assume_utc())
-    })
+    OffsetDateTime::parse(s, &Rfc3339)
+        .or_else(|_| {
+            Date::parse(s, &Iso8601::DATE).map(|d| d.with_time(Time::MIDNIGHT).assume_utc())
+        })
+        .or_else(|_| PrimitiveDateTime::parse(s, DATETIME_NO_SECS).map(|dt| dt.assume_utc()))
 }
