b0c4413b16491ecbfb0480a1ae8d3753f1ff248b
Slim BEJI <mslimbeji@gmail.com>
2026-05-24 17:29:21 +0100

    fix cron

--- backend-axum/src/background/crons/emails.rs
+++ backend-axum/src/background/crons/emails.rs
@@ -4,7 +4,7 @@ use apalis_cron::Tick;
 use crate::{background::publishers, lib_::clients::HandlerError, services::SharedState};
 
 pub const SEND_NEWSLETTER_TASKNAME: &str = "send_newsletter_task";
-pub const SEND_NEWSLETTER_CRON: &str = "0 * * * *";
+pub const SEND_NEWSLETTER_CRON: &str = "0 0 * * * *";
 
 pub async fn send_newsletter_task(_: Tick, data: Data<SharedState>) -> Result<(), HandlerError> {
     let result = publishers::send_newsletter(
