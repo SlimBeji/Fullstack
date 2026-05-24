7735a9daf16755f16976231bc65111e5be814416
Slim BEJI <mslimbeji@gmail.com>
2026-05-24 17:29:35 +0100

    add scheduler worker

--- backend-axum/src/main.rs
+++ backend-axum/src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use backend::background::crons::create_scheduler;
 use backend::background::handlers::create_worker;
 use backend::services::setup;
 use tokio::net::TcpListener;
@@ -16,6 +17,7 @@ async fn main() {
     // Creating state, worker, app and http listener
     let app_state = Arc::new(AppState::new().await);
     let worker = create_worker(app_state.clone());
+    let scheduler = create_scheduler(app_state.clone());
     let app = api::get_app().with_state(app_state.clone());
     let listener = TcpListener::bind(config::ENV.bind_addr())
         .await
@@ -25,6 +27,7 @@ async fn main() {
     let (tx, _) = tokio::sync::broadcast::channel::<()>(1);
     let mut axum_shutdown = tx.subscribe();
     let mut worker_shutdown = tx.subscribe();
+    let mut scheduler_shutdown = tx.subscribe();
 
     // Start Axum server
     let mut axum_handle = tokio::spawn(async move {
@@ -43,11 +46,20 @@ async fn main() {
         }
     });
 
+    // Start Apalis Scheduler
+    let mut scheduler_handle = tokio::spawn(async move {
+        tokio::select! {
+            res = scheduler.run() => res,
+            _ = scheduler_shutdown.recv() => Ok(()), // Stop if termination signal received
+        }
+    });
+
     // Poll Worker and HttpServer while waiting for shutdown_signal
     tokio::select! {
         _ = shutdown_signal() => {}
         _ = &mut axum_handle => {}
         _ = &mut worker_handle => {}
+        _ = &mut scheduler_handle => {}
     }
 
     // Send termination signal to the sibscribers and await future
@@ -57,6 +69,9 @@ async fn main() {
     }
     if !worker_handle.is_finished() {
         let _ = worker_handle.await;
+    }
+    if !scheduler_handle.is_finished() {
+        let _ = scheduler_handle.await;
     }
 
     // Gracefull cleaning of the state
