f0ac6c8f56ca5d5c9177f942df19bb21641efb7f
Slim BEJI <mslimbeji@gmail.com>
2026-05-30 23:10:07 +0100

    fix vue linting

--- frontend-vue/src/router/index.ts
+++ frontend-vue/src/router/index.ts
@@ -39,7 +39,7 @@ const router = createRouter({
     ],
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
     const authStore = useAuthStore();
     if (!authStore.isLoggedIn) {
         // Check the local storage if no token found
