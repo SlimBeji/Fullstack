e5a8bfe949fbeae004ceec705cc29c2979a76845
Slim BEJI <mslimbeji@gmail.com>
2026-06-07 10:51:11 +0100

    remove unsued types

--- frontend-vue/src/types/html.ts
+++ frontend-vue/src/types/html.ts
@@ -1,5 +1,3 @@
 export type ButtonType = "button" | "submit" | "reset" | undefined;
 
-export type FormSubmitHandler = (e: SubmitEvent) => void;
-
 export type CssClass = string | Record<string, boolean> | CssClass[];
