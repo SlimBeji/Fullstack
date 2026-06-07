d574d2432d982950ea496c7bd26142761021d840
Slim BEJI <mslimbeji@gmail.com>
2026-06-07 11:50:35 +0100

    register dummy routes

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import type { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Auth } from '@/pages/auth/auth';
+import { NewPlace } from '@/pages/new-place/new-place';
+import { UpdatePlace } from '@/pages/update-place/update-place';
+import { UserPlaces } from '@/pages/user-places/user-places';
+import { Users } from '@/pages/users/users';
+
+export const routes: Routes = [
+    { path: 'auth', component: Auth },
+    { path: '', component: Users },
+    { path: ':userId/places', component: UserPlaces },
+    { path: 'places/new', component: NewPlace },
+    { path: 'places/:placeId', component: UpdatePlace },
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
