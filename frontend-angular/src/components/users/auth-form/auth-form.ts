a316ee63ffb2a0b92026438e310c02f462a3391a
Slim BEJI <mslimbeji@gmail.com>
2026-06-07 17:57:02 +0100

    create an auth form component

---
+++ frontend-angular/src/components/users/auth-form/auth-form.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-auth-form',
+    templateUrl: './auth-form.html',
+})
+export class AuthForm {}
--- frontend-angular/src/pages/auth/auth.ts
+++ frontend-angular/src/pages/auth/auth.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
+import { AuthForm } from '@/components/users';
+
 @Component({
     selector: 'app-auth',
-    imports: [],
+    imports: [AuthForm],
     templateUrl: './auth.html',
 })
 export class Auth {}
