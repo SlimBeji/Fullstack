b8dc4e92a2b81857d76bec4cb9ebe7d0c018a028
Slim BEJI <mslimbeji@gmail.com>
2026-06-10 15:56:20 +0100

    add MainNavigation to the App html

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MainNavigation } from '@/components/navigation';
+
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, MainNavigation],
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
