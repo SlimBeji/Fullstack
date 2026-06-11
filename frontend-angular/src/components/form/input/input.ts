28faace02127966da63b5d4dd5758c2ce66db14d
Slim BEJI <mslimbeji@gmail.com>
2026-06-11 17:26:42 +0100

    update input component

--- frontend-angular/src/components/form/input/input.ts
+++ frontend-angular/src/components/form/input/input.ts
@@ -1,4 +1,5 @@
-import { Component, computed, input, model, signal } from '@angular/core';
+import { Component, computed, input, model } from '@angular/core';
+import type { ValidationError, WithOptionalFieldTree } from '@angular/forms/signals';
 
 import type { CssClass } from '@/types';
 
@@ -8,27 +9,26 @@ import type { CssClass } from '@/types';
     styleUrl: './input.css',
 })
 export class Input {
-    // Model
-    value = model.required<string>();
+    // Form
+    value = model<string>();
+    invalid = input<boolean>();
+    touched = input<boolean>();
+    disabled = input<boolean>();
+    errors = input<readonly WithOptionalFieldTree<ValidationError>[]>();
 
     // Inputs
     id = input.required<string>();
     label = input.required<string>();
-    isValid = input<boolean>();
     customClass = input<CssClass>('basis-full');
     element = input<'input' | 'textarea'>('input');
     type = input<HTMLInputElement['type']>();
     step = input<string>();
-    disabled = input<boolean>(false);
     rows = input<number>(3);
-    placeholder = input<string>();
-    errorText = input<string>('The input is not valid');
-
-    // Signals
-    isTouched = signal<boolean>(false);
+    placeholder = input<string>('');
+    errorText = input<string>();
 
     // Computed
-    showError = computed(() => !this.isValid() && this.isTouched());
+    showError = computed(() => !!this.invalid() && !!this.touched());
 
     inputClass = computed(() => ({
         disabled: this.disabled(),
@@ -39,12 +39,13 @@ export class Input {
 
     isTextarea = computed(() => this.element() === 'textarea');
 
-    // Handlers
-    onInput(event: InputEvent): void {
-        this.value.set((event.target as HTMLInputElement).value);
-    }
+    errorMessage = computed(() => {
+        const errorText = this.errorText();
+        if (errorText) return errorText;
 
-    onBlur(): void {
-        if (!this.disabled()) this.isTouched.set(true);
-    }
+        const errors = this.errors();
+        if (errors && errors.length > 0) return errors[0].message;
+
+        return 'The input is not valid';
+    });
 }
