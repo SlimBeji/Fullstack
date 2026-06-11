e7b4c081c6adcc0b99066c5da75f5f975e2c2d9e
Slim BEJI <mslimbeji@gmail.com>
2026-06-11 11:29:01 +0100

    create button component

---
+++ frontend-angular/src/components/form/button/button.ts
@@ -0,0 +1,37 @@
+import { Component, computed, input, output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import type { ButtonType, CssClass } from '@/types';
+
+@Component({
+    selector: 'app-button',
+    templateUrl: './button.html',
+    styleUrl: './button.css',
+    imports: [RouterLink],
+})
+export class Button {
+    // Inputs
+    disabled = input<boolean>(false);
+    inverse = input<boolean>(false);
+    color = input<'primary' | 'secondary' | 'success' | 'warning' | 'danger'>('primary');
+    customClass = input<CssClass>('');
+    to = input<string>();
+    href = input<string>();
+    type = input<ButtonType>('button');
+
+    // Outputs
+    clicked = output<void>();
+
+    // Computed
+    inverseClass = computed(() => (this.inverse() && !this.disabled() ? 'inverse' : ''));
+    colorClass = computed(() => (this.disabled() ? 'disabled' : this.color()));
+    isAnchor = computed(() => !!this.href() && !this.to());
+    isRouterLink = computed(() => !!this.to() && !this.href());
+
+    // Handlers
+    clickHandler() {
+        if (!this.disabled()) {
+            this.clicked.emit();
+        }
+    }
+}
