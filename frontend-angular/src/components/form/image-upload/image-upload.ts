5a68ea6d8b869a56526273edd22e8abad99b55ba
Slim BEJI <mslimbeji@gmail.com>
2026-06-11 17:34:48 +0100

    update the image-upload component

--- frontend-angular/src/components/form/image-upload/image-upload.ts
+++ frontend-angular/src/components/form/image-upload/image-upload.ts
@@ -15,14 +15,15 @@ export class ImageUpload {
     // Template refs
     filePicker = viewChild.required<ElementRef<HTMLInputElement>>('filePicker');
 
-    // Models
-    data = model.required<{ file: File | null; url: string }>();
+    // Form
+    value = model<{ file: File | null; url: string }>();
+    invalid = input<boolean>();
+    touched = input<boolean>();
+    disabled = input<boolean>(false);
 
     // Inputs
     id = input.required<string>();
-    isValid = input<boolean>();
     buttonText = input<string>('Pick an image');
-    disabled = input<boolean>(false);
     inverse = input<boolean>(false);
     color = input<'primary' | 'secondary' | 'success' | 'warning' | 'danger'>('primary');
     errorText = input<string>();
@@ -36,22 +37,22 @@ export class ImageUpload {
 
     colorClass = computed(() => (this.disabled() ? 'disabled' : this.color()));
 
-    showError = computed(() => (!this.isValid() || !!this.uploadError()) && this.uploadAttempt());
+    showError = computed(() => (this.invalid() || !!this.uploadError()) && this.uploadAttempt());
 
     // Hanlders
     async changeHandler(event: Event): Promise<void> {
         this.uploadAttempt.set(true);
         const files = (event.target as HTMLInputElement).files;
         if (!files || files.length === 0) {
-            this.data.update((d) => ({ ...d, file: null, url: '' }));
+            this.value.update((d) => ({ ...d, file: null, url: '' }));
             this.uploadError.set('Something went wrong! No file found!');
         } else if (files.length > 1) {
-            this.data.update((d) => ({ ...d, file: null, url: '' }));
+            this.value.update((d) => ({ ...d, file: null, url: '' }));
             this.uploadError.set('Please upload only one file at a time!');
         } else {
             try {
                 const url = await fileToUrl(files[0]);
-                this.data.update((d) => ({ ...d, file: files[0], url }));
+                this.value.update((d) => ({ ...d, file: files[0], url }));
                 this.uploadError.set('');
             } catch {
                 this.uploadError.set('Uploaded file corrupted');
