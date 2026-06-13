a247855e580b00c38e4885def2f74581c8332416
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 18:09:12 +0100

    create places-list component

--- frontend-angular/src/components/place/index.ts
+++ frontend-angular/src/components/place/index.ts
@@ -1,1 +1,2 @@
 export { PlaceItem } from './place-item/place-item';
+export { PlacesList } from './places-list/places-list';
---
+++ frontend-angular/src/components/place/places-list/places-list.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+
+import { Button } from '@/components/form';
+import { AppRoute } from '@/router/routes';
+import type { Place } from '@/types';
+
+import { PlaceItem } from '../place-item/place-item';
+
+@Component({
+    selector: 'app-places-list',
+    templateUrl: './places-list.html',
+    styleUrl: './places-list.css',
+    imports: [Button, PlaceItem],
+    host: { class: 'contents' },
+})
+export class PlacesList {
+    protected readonly AppRoute = AppRoute;
+
+    // Inputs
+    sameAuthenticatedUser = input<boolean>();
+    items = input.required<Place[]>();
+
+    // Outputs
+    delete = output<void>();
+}
