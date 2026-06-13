b60fec74826be319dd37b062fe7d5db81ae9e190
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 17:45:30 +0100

    create Map component

--- frontend-angular/src/components/ui/index.ts
+++ frontend-angular/src/components/ui/index.ts
@@ -3,4 +3,5 @@ export { Backdrop } from './backdrop/backdrop';
 export { ErrorModal } from './error-modal/error-modal';
 export { HttpError } from './http-error/http-error';
 export { LoadingSpinner } from './loading-spinner/loading-spinner';
+export { Map } from './map/map';
 export { Modal } from './modal/modal';
---
+++ frontend-angular/src/components/ui/map/map.ts
@@ -0,0 +1,52 @@
+import type { ElementRef, OnDestroy } from '@angular/core';
+import { afterNextRender, Component, effect, input, viewChild } from '@angular/core';
+import type { Map as LeafletMap, Marker } from 'leaflet';
+import * as L from 'leaflet';
+
+import type { Location } from '@/types';
+
+@Component({
+    selector: 'app-map',
+    templateUrl: './map.html',
+    styleUrl: './map.css',
+})
+export class Map implements OnDestroy {
+    // Init
+    private map: LeafletMap | null = null;
+    private marker: Marker | null = null;
+
+    // ViewChild
+    private mapDiv = viewChild.required<ElementRef<HTMLDivElement>>('mapDiv');
+
+    // Inputs
+    position = input.required<Location>();
+    zoom = input.required<number>();
+    markerText = input.required<string>();
+
+    // Handlers
+    private readonly _onMount = afterNextRender(() => {
+        const pos = this.position();
+        this.map = L.map(this.mapDiv().nativeElement).setView([pos.lat, pos.lng], this.zoom());
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }).addTo(this.map);
+
+        this.marker = L.marker([pos.lat, pos.lng]).addTo(this.map).bindPopup(this.markerText());
+
+        this.map.invalidateSize();
+    });
+
+    private readonly _onUpdate = effect(() => {
+        const pos = this.position();
+        if (this.map && this.marker) {
+            this.marker.setLatLng([pos.lat, pos.lng]);
+            this.map.setView([pos.lat, pos.lng], this.zoom());
+        }
+    });
+
+    ngOnDestroy() {
+        this.map?.remove();
+    }
+}
