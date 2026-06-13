01a7ef02f7fee2baf545d130a52953efcc2d2537
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 12:24:28 +0100

    use teleport with side drawer

--- frontend-angular/src/components/navigation/side-drawer/side-drawer.ts
+++ frontend-angular/src/components/navigation/side-drawer/side-drawer.ts
@@ -1,9 +1,13 @@
+import { CdkPortal } from '@angular/cdk/portal';
 import { Component, input, output } from '@angular/core';
+
+import { Teleport } from '@/services';
 
 @Component({
     selector: 'app-side-drawer',
     templateUrl: './side-drawer.html',
     styleUrl: './side-drawer.css',
+    imports: [CdkPortal, Teleport],
 })
 export class SideDrawer {
     show = input<boolean>(false);
