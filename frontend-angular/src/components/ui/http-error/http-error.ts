4e0d6bc09a3af22d4ebd6eb7e3c29dd5b470d9ed
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 15:17:13 +0100

    create a HttpError component

---
+++ frontend-angular/src/components/ui/http-error/http-error.ts
@@ -0,0 +1,41 @@
+import { Component, computed, inject, input, output } from '@angular/core';
+import type { AxiosResponse } from 'axios';
+
+import { Button } from '@/components/form';
+import { AuthStore } from '@/store';
+
+import { ErrorModal } from '../error-modal/error-modal';
+import { Modal } from '../modal/modal';
+
+interface ErrorInput {
+    tokenExpired?: boolean;
+    message?: string;
+    response?: AxiosResponse;
+}
+
+@Component({
+    selector: 'app-http-error',
+    templateUrl: './http-error.html',
+    styleUrl: './http-error.css',
+    imports: [Modal, ErrorModal, Button],
+})
+export class HttpError {
+    // Init
+    authStore = inject(AuthStore);
+
+    // Inputs
+    error = input<ErrorInput>();
+    header = input.required<string>();
+
+    // Outputs
+    closed = output<void>();
+
+    // Computed
+    isTokenExpired = computed(() => this.error()?.tokenExpired);
+
+    // Handlers
+    tokenExpiredCleaner() {
+        this.closed.emit();
+        this.authStore.logout();
+    }
+}
--- frontend-angular/src/components/ui/index.ts
+++ frontend-angular/src/components/ui/index.ts
@@ -1,4 +1,5 @@
 export { Backdrop } from './backdrop/backdrop';
 export { ErrorModal } from './error-modal/error-modal';
+export { HttpError } from './http-error/http-error';
 export { LoadingSpinner } from './loading-spinner/loading-spinner';
 export { Modal } from './modal/modal';
