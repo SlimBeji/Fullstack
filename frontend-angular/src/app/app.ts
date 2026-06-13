4a1b9fb75d237678c331a73b15e83ca8736c11e4
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 12:18:07 +0100

    update the app component to use the teleport-outlet directive

--- frontend-angular/src/app/app.ts
+++ frontend-angular/src/app/app.ts
@@ -1,11 +1,13 @@
+import { CdkPortalOutlet } from '@angular/cdk/portal';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { MainNavigation } from '@/components/navigation';
+import { TeleportOutlet } from '@/services';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, MainNavigation],
+    imports: [RouterOutlet, MainNavigation, CdkPortalOutlet, TeleportOutlet],
     templateUrl: './app.html',
     styleUrl: './app.css',
 })
