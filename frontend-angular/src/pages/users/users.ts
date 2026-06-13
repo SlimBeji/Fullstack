0a72bd41e19c2980448ac02123a1e669b757e55a
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 15:49:22 +0100

    update users page

--- frontend-angular/src/pages/users/users.ts
+++ frontend-angular/src/pages/users/users.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+
+import { HttpError, LoadingSpinner } from '@/components/ui';
+import { UsersList } from '@/components/user';
+import { BackendService } from '@/services';
+import type { User } from '@/types';
 
 @Component({
     selector: 'app-users',
-    imports: [],
     templateUrl: './users.html',
+    imports: [HttpError, LoadingSpinner, UsersList],
 })
-export class Users {}
+export class Users implements OnInit {
+    // Init
+    backend = inject(BackendService);
+
+    // Computed
+    items = computed((): User[] => {
+        if (this.backend.httpData().json?.data) {
+            return this.backend.httpData().json?.data as User[];
+        }
+        return [];
+    });
+
+    // Events
+    ngOnInit(): void {
+        this.backend.sendRequest('/users/', 'get');
+    }
+}
