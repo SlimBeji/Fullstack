467803afd4140a2f5b4b187ab3629bd1d72aaf3a
Slim BEJI <mslimbeji@gmail.com>
2026-06-13 12:18:39 +0100

    use teleport with backdrop

--- frontend-angular/src/components/ui/backdrop/backdrop.ts
+++ frontend-angular/src/components/ui/backdrop/backdrop.ts
@@ -1,9 +1,13 @@
+import { CdkPortal } from '@angular/cdk/portal';
 import { Component, output } from '@angular/core';
+
+import { Teleport } from '@/services';
 
 @Component({
     selector: 'app-backdrop',
     templateUrl: './backdrop.html',
     styleUrl: './backdrop.css',
+    imports: [CdkPortal, Teleport],
 })
 export class Backdrop {
     clicked = output<void>();
