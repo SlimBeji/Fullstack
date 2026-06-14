2f231ff6bdf7b51e220a67680690c6756a6c8485
Slim BEJI <mslimbeji@gmail.com>
2026-06-14 12:00:24 +0100

    renable encapsulation for NavLinks

--- frontend-angular/src/components/navigation/nav-links/nav-links.ts
+++ frontend-angular/src/components/navigation/nav-links/nav-links.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { AppRoute, userPlacesRoute } from '@/router';
@@ -9,7 +9,6 @@ import { AuthStore } from '@/store';
     templateUrl: './nav-links.html',
     styleUrl: './nav-links.css',
     imports: [RouterLink, RouterLinkActive],
-    encapsulation: ViewEncapsulation.None,
 })
 export class NavLinks {
     store = inject(AuthStore);
