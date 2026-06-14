7a8426789843afae7197d8d8a4bd5c44592f81ec
Slim BEJI <mslimbeji@gmail.com>
2026-06-14 11:25:24 +0100

    update the place update page

--- frontend-angular/src/pages/update-place/update-place.ts
+++ frontend-angular/src/pages/update-place/update-place.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+
+import { PlaceUpdateForm } from '@/components/place';
 
 @Component({
     selector: 'app-update-place',
-    imports: [],
     templateUrl: './update-place.html',
+    imports: [PlaceUpdateForm],
 })
-export class UpdatePlace {}
+export class UpdatePlace {
+    // Init
+    route = inject(ActivatedRoute);
+    private params = toSignal(this.route.params);
+
+    // Computed
+    placeId = computed(() => this.params()?.['placeId']);
+}
