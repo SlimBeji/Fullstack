a67af6a61a33820a1dcc1a98f44b966fefa3f691
Slim BEJI <mslimbeji@gmail.com>
2026-06-14 23:17:59 +0100

    scope user-item style

--- frontend-react/src/components/user/UserItem.tsx
+++ frontend-react/src/components/user/UserItem.tsx
@@ -1,10 +1,10 @@
-import "./UserItem.css";
-
 import { Link } from "react-router-dom";
 
 import { Avatar } from "@/components/ui";
 import { userPlacesRoute } from "@/router";
 import type { User } from "@/types";
+
+import styles from "./UserItem.module.css";
 
 const placeholder = "/public/avatar_placeholder.jpg";
 
@@ -20,16 +20,19 @@ const UserItem: React.FC<UserItemProps> = ({ user }) => {
     }
 
     return (
-        <li className="user-item">
+        <li className={styles["user-item"]}>
             <div className="card">
-                <Link to={userPlacesRoute(user.id)} className="user-item-link">
-                    <div className="user-avatar">
+                <Link
+                    to={userPlacesRoute(user.id)}
+                    className={styles["user-item-link"]}
+                >
+                    <div className={styles["user-avatar"]}>
                         <Avatar
                             imageUrl={user.image_url || placeholder}
                             alt={user.name}
                         />
                     </div>
-                    <div className="user-info">
+                    <div className={styles["user-info"]}>
                         <h2>{user.name}</h2>
                         <h3>{placeNumber}</h3>
                     </div>
